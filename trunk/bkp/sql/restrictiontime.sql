e1c5c9557beda4c0402f97989e593cb6d8078f8f
lucas <devnull@localhost>
2009-08-31 21:19:10 +0000

    Adicionando e removendo arquivos

--- trunk/bkp/sql/restrictiontime.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO bkp_restrictiontime (id,restriction_time) VALUES (1,'00:01');
-INSERT INTO bkp_restrictiontime (id,restriction_time) VALUES (2,'08:00');
-INSERT INTO bkp_restrictiontime (id,restriction_time) VALUES (3,'12:00');
-INSERT INTO bkp_restrictiontime (id,restriction_time) VALUES (4,'14:00');
-INSERT INTO bkp_restrictiontime (id,restriction_time) VALUES (5,'18:00');
--- trunk/bkp/sql/storage.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO `bkp_storage` VALUES (1,'StorageLocal','192.168.50.3',9103,'OYkqZCiCbCqBTht4p8ZF','Storage Local Nimbus');
-
