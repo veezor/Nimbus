c5922e34a5c20a1d4a7d3f2113b8033ba80b3d58
lucas <devnull@localhost>
2009-09-17 04:23:11 +0000

    Primeira versao funcionando

--- NimbusConsoleMenu/NimbusConsoleMenu.py
+++ NimbusConsoleMenu/NimbusConsoleMenu.py
@@ -1,4 +1,4 @@
-#! /usr/bin/env python
+#! /usr/bin/env python2.4
 # -*- coding: iso-8859-1 -*-
 import os
 from SOAPpy import SOAPProxy
@@ -12,7 +12,7 @@ def ConsoleMenu(str_mn,title='Nimbus Config System'):
 		for opt in str_mn["menu"]:
 			print opt
 	elif str_mn["tp"] == "input":
-		str_menu["menu"]
+		exec str_mn["menu"]
 	sel = raw_input("nimbus:~#")
 	return str_mn["optout"][sel]
 	
@@ -26,21 +26,24 @@ def netinfo():
 	return outlst
 
 def netgenerate():
-	while true:
-		net_ip = raw_input("IP:")
-		net_broad = raw_input("Broadcast:")
-		net_mask = raw_input("Netmask:")
-		net_confirm = raw_input("Confirm: Y/n :~#")
-		if net_confirm == "Y":
-			server.generate_interfaces("eth0", net_ip, net_mask, type="static", broad=net_broad)
-			break
-	
+    net_confirm = "N"
+    while net_confirm == "N":
+        net_ip = raw_input("IP:")
+        net_broad = raw_input("Broadcast:")
+        net_mask = raw_input("Netmask:")
+        net_confirm = raw_input("Confirm: Y/n :~#")
+        if net_confirm == "Y":
+            server.generate_interfaces("eth0", net_ip, net_mask, type="static", broad=net_broad)
+            break
+    print "1 - Back Menu / 2 - Main Menu"
+
 menu_principal = ["1 - Network Settings","2 - Nimbus Backup Server Product Activation"]
 str_principal = {"id":1,"tp":"list","menu":menu_principal,"optout":{"1":2,"2":3}}
 menu_netset = ["1 - View Network Settings","2 - Configure Network"]
 str_netset = {"id":2,"tp":"list","menu":menu_netset,"optout":{"1":4,"2":5}}
-str_vnetset = {"id":4,"tp":"list","menu":netinfo(),"optout":{"1":2,"2":2}}
-str_netgen = {"id":5,"tp":"input","menu":netgenerate(),"optout":{"1":2,"2":2}}
+str_vnetset = {"id":4,"tp":"list","menu":netinfo(),"optout":{"1":2,"2":1}}
+str_netgen = {"id":5,"tp":"input","menu":"netgenerate()","optout":{"1":2,"2":1}}
+
 
 console = {1:str_principal,2:str_netset,4:str_vnetset,5:str_netgen}
 opt = 1
