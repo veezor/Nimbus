ac51e1a28a6971fb28d890ee03759e219d67edb6
Rodrigo Araújo <fenrrir@gmail.com>
2009-12-16 18:48:03 -0300

    Inserindo testes dos procedures

--- django/backup_corporativo/bkp/tests.py
+++ django/backup_corporativo/bkp/tests.py
@@ -21,6 +21,7 @@ class NimbusTest(TestCase):
         except OSError, e:
             pass
 
+
     def get(self, url):
         response = self.client.get(url, follow=True)
         self.failUnlessEqual(response.status_code, 200, 
@@ -45,7 +46,8 @@ class NimbusTest(TestCase):
         for key,value in data.items():
             attr = getattr( item, key )
             self.assertEqual( attr, value, 
-                             "Field %s: %s != %s" % (itemname, attr, value) )
+                             "Field %s.%s: %s != %s" % ( itemname, key, 
+                                                         attr, value) )
         return response
 
 
@@ -169,6 +171,126 @@ class NimbusViewTest(NimbusTest):
         response = self.post( "/computer/1/config/dump", {})
         response = self.get( "/computer/1/config/")
 
+    def test_procedure_create(self):
+        self.test_computer_create()
+        self.test_system_config_update()
+        self.post( "/computer/1/backup/create",
+                   dict(offsite_on=True,
+                        procedure_name="test",
+                        storage=1,
+                        path="c:/test/"))
+        self.assertEqual( len(models.Procedure.objects.all()), 1  )
+        self.assertEqual( len(models.FileSet.objects.all()), 1  )
+
+
+    def test_procedure_update(self):
+        self.test_procedure_create()
+        self.get("/procedure/1/backup/edit")
+        response = self.post( "/procedure/1/backup/update",
+                              dict( procedure_name="test2",
+                                    offsite_on=False,
+                                    storage=1))
+
+        procedure = response.context['proc']
+        self.assertEqual(procedure.procedure_name, "test2")
+        self.assertEqual(procedure.offsite_on, False)
+        self.assertEqual(procedure.storage.id, 1)
+
+
+    def test_procedure_delete(self):
+        self.test_procedure_create()
+        self.get("/procedure/1/delete")
+        self.post("/procedure/1/delete", {})
+        self.assertEqual( len(models.Procedure.objects.all()), 0  )
+
+    def test_fileset_new(self):
+        path = "c:/test2/"
+        self.test_procedure_create()
+        self.get("/procedure/1/fileset/new")
+        self.post("/procedure/1/fileset/create", 
+                  {"path" : path} )
+        self.assertEqual( len(models.FileSet.objects.all()), 2  )
+        fileset = models.FileSet.objects.get(pk=2)
+        self.assertEqual( fileset.path, path )
+
+
+    def test_schedule_new(self):
+        self.test_procedure_create()
+        self.get("/procedure/1/schedule/new")
+
+        response = self.post("/procedure/1/schedule/create",
+                             dict( level="Incremental",
+                                   schedule_type="Weekly",
+                                   hour="00:00:00",
+                                   sunday=True ))
+        self.assertEqual( len(models.Schedule.objects.all()), 1  )
+        self.assertEqual( len(models.WeeklyTrigger.objects.all()), 1  )
+
+        
+        trigger = models.WeeklyTrigger.objects.get(pk=1)
+        self.assertTrue(trigger.sunday)
+
+        self.assertEqual(trigger.level, "Incremental")
+
+        response = self.post("/procedure/1/schedule/create",
+                             dict( level="Incremental",
+                                   schedule_type="Monthly",
+                                   hour="00:00:00",
+                                   target_days="1;2;3;4;5;6" ))
+        self.assertEqual( len(models.Schedule.objects.all()), 2  )
+        self.assertEqual( len(models.MonthlyTrigger.objects.all()), 1  )
+
+
+    def test_fileset_delete(self):
+        self.test_fileset_new()
+        self.get("/fileset/1/delete")
+        size = len(models.FileSet.objects.all())
+        self.post("/fileset/1/delete", {})
+        new_size = len(models.FileSet.objects.all())
+        self.assertEqual(new_size, size - 1)
+
+
+    def test_schedule_update(self):
+        self.test_schedule_new()
+        self.get("/schedule/1/edit")
+
+        response = self.post("/schedule/1/update",
+                             dict( level="Full",
+                                   hour="01:00:00",
+                                   monday=True ))
+
+        trigger = models.WeeklyTrigger.objects.get(pk=1)
+        self.assertTrue(trigger.monday)
+        self.assertEqual(trigger.level, "Full")
+
+        response = self.post("/schedule/2/update",
+                             dict( level="Full",
+                                   hour="01:00:00",
+                                   target_days="5;6" ))
+
+
+        trigger = models.MonthlyTrigger.objects.get(pk=1)
+        self.assertEqual(trigger.level, "Full")
+        self.assertEqual(trigger.target_days, "5;6")
+
+
+    def test_schedule_delete(self):
+        self.test_schedule_new()
+        self.get("/schedule/1/delete")
+
+        size = len(models.Schedule.objects.all())
+        self.post("/schedule/1/delete", {})
+        new_size = len(models.Schedule.objects.all())
+        self.assertEqual(new_size, size - 1)
+
+
+    def test_restore_new(self):
+        self.test_computer_create()
+        self.get("/restore/new")
+
+        response = self.post("/restore/create", 
+                             dict(target_client=1))
+
 
 
     def test_system_password_edit(self):
@@ -195,6 +317,7 @@ class NimbusViewTest(NimbusTest):
         self.gets( [ "/session/",
                      "/session/new",
                      "/session/delete"] )
+
         self.post("/session/delete", {})
 
         self.post("/session/", 
