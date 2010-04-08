61c112f76a7b590e3f5ed5fbfd544692202160b9
Rodrigo Araújo <fenrrir@gmail.com>
2010-04-08 17:13:47 -0300

    Gateway agora recebe parametros como caminho dos arquivos e md5 via post

--- webservices/gateway/urls.py
+++ webservices/gateway/urls.py
@@ -7,9 +7,9 @@ admin.autodiscover()
 from gateway.nimbusgate.views import handler
 
 urlpatterns = patterns('gateway.nimbusgate.views',
-    url(r'json/get/key/(?P<key>.*)$', handler.get),
-    url(r'json/put/base64/(?P<base64_of_md5>\w+)/key/(?P<key>.*)$', handler.put),
-    url(r'json/delete/key/(?P<key>.*)$', handler.delete),
+    url(r'json/get$', handler.get),
+    url(r'json/put$', handler.put),
+    url(r'json/delete$', handler.delete),
     url(r'json/list$', handler.list),
 
     # Uncomment the next line to enable the admin:
