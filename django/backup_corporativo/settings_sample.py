7b88c0f952e9af187248b5754cf931898c885885
Rodrigo Araújo <fenrrir@gmail.com>
2010-04-09 17:13:27 -0300

    Correção de BUG no settings_sample.py. Import do os.path.join errado

--- django/backup_corporativo/settings_sample.py
+++ django/backup_corporativo/settings_sample.py
@@ -53,7 +53,7 @@ USE_I18N = True
 
 # Absolute path to the directory that holds media.
 # Example: "/home/media/media.lawrence.com/"
-MEDIA_ROOT = join( dirname(__file__), 'static')
+MEDIA_ROOT = os.path.join( dirname(__file__), 'static')
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash if there is a path component (optional in other cases).
