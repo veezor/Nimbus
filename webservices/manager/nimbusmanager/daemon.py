873c7c1caec80f8b2ca24933e5e3f76c59e1434d
Rodrigo Araújo <fenrrir@gmail.com>
2010-04-09 14:28:16 -0300

    Resolução do BUG onde o daemon do nimbusmanager parava de funcionar

--- webservices/manager/nimbusmanager/daemon.py
+++ webservices/manager/nimbusmanager/daemon.py
@@ -23,33 +23,8 @@ def main(debug=False):
 
         config = util.get_config()
         pidfile = config.get("PATH","pid")
-        # do the UNIX double-fork magic, see Stevens' "Advanced
-        # Programming in the UNIX Environment" for details (ISBN 0201563177)
-        try:
-            pid = os.fork()
-            if pid > 0:
-                # exit first parent
-                sys.exit(0)
-        except OSError, e:
-            logger.error("fork #1 failed: %d (%s)" % (e.errno, e.strerror))
-            sys.exit(1)
-
-        # decouple from parent environment
-        os.chdir("/")   #don't prevent unmounting....
-        os.setsid()
-        os.umask(0)
-
-        # do second fork
-        try:
-            pid = os.fork()
-            if pid > 0:
-                open(pidfile,'w').write("%d"%pid)
-                sys.exit(0)
-        except OSError, e:
-            logger.error("fork #2 failed: %d (%s)" % (e.errno, e.strerror))
-            sys.exit(1)
-
-        # start the daemon main loop
+        pid = os.getpid()
+        open(pidfile,'w').write("%d"%pid)
 
         manager.Manager(debug).run()
     except Exception, ex:
