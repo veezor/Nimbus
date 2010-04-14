bda704a672101a781326bc539de3ddfcc2799746
Rodrigo Araújo <fenrrir@gmail.com>
2010-04-14 18:08:41 -0300

    Logando exceções na geração dos arquivos de configuração

--- django/backup_corporativo/bkp/template.py
+++ django/backup_corporativo/bkp/template.py
@@ -1,14 +1,21 @@
 from django.template.loader import render_to_string
 from codecs import open as file
+import logging
 
 
 def render_to_file(filename, template, **kwargs):
     try:
+        logger = logging.getLogger()
         f = file(filename, "w", encoding="utf-8")
         content = render_to_string(template, kwargs)
-        f.write(content) 
-    finally:
-        f.close()
+        try:
+            f.write(content) 
+        except Exception, e:
+            logger.exception("Erro ao gerar arquivo %s" % filename)
+        finally:
+            f.close()
+    except Exception, e:
+        logger.exception("Erro ao abrir arquivo %s" % filename)
 
 
 
