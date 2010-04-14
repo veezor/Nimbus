d2d0bf6d0c929bf5d477d53d534d115541a06bb7
Rodrigo Araújo <fenrrir@gmail.com>
2010-04-14 18:02:17 -0300

    Adicionando UUID aos models device e resolvendo pegando bug no uso do super

--- django/backup_corporativo/bkp/app_models/device.py
+++ django/backup_corporativo/bkp/app_models/device.py
@@ -6,11 +6,15 @@ class Device(models.Model):
     name = models.CharField(max_length=255)
     archive = models.FilePathField()
     storage = models.ForeignKey(Storage)
+    nimbus_uuid = models.ForeignKey(NimbusUUID, default=-1)
 
     def save(self, *args, **kwargs):
         NimbusUUID.generate_uuid_or_leave(self)
-        super(Procedure, self).save(*args, **kwargs)
-
+        super(Device, self).save(*args, **kwargs)
 
     def device_bacula_name(self):
         return "%s_device" % self.nimbus_uuid.uuid_hex
+
+
+    class Meta:
+        app_label = 'bkp'
