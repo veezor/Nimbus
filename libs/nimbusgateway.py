6ffa29f56b2adaec6f8b286887b12beaed1ca59b
Rodrigo Araújo <fenrrir@gmail.com>
2010-04-24 00:32:47 +0700

    Suporte a listagem de arquivos na biblioteca cliente do gateway

--- libs/nimbusgateway.py
+++ libs/nimbusgateway.py
@@ -11,8 +11,17 @@ import urlparse
 import pycurl
 import md5
 import os
+import S3
 
+class _S3HttpResponseFake(object):
 
+    def __init__(self, content):
+        self.status = 200
+        self.reason = "OK"
+        self.content = content
+
+    def read(self):
+        return self.content
 
 class _File(object):
 
@@ -63,21 +72,29 @@ class Api(object):
         self.encoding = encoding
         self.url = gateway_url
 
-    def _fetch_url(self, url, **post_data):
-        # Get a url opener that can handle basic auth
-        opener = self._get_opener(url)
 
-        encoded_post_data = self._encode_post_data(post_data)
-
-        url_data = opener.open(url, encoded_post_data).read()
-        opener.close()
+    def _fetch_json_url(self, url, **post_data):
+        url_data = self._fetch_url(url, **post_data)
         data = simplejson.loads(url_data)
         return data
+
+
+    def _fetch_url(self, url, **post_data):
+
+        opener = self._get_opener(url)
+        encoded_post_data = self._encode_post_data(post_data)
+        url_data = opener.open(url, encoded_post_data).read()
+        opener.close()
+
+        return url_data
+
+
    
     def _encode_post_data(self, post_data):
         if not post_data:
             return None
-        return urllib.urlencode( dict( [ (k, self._encode(v)) for k, v in post_data.items()]))
+        return urllib.urlencode( dict( [ (k, self._encode(v)) \
+                                    for k, v in post_data.items()]))
 
     def _encode(self, data):
         if self.encoding:
@@ -88,29 +105,45 @@ class Api(object):
     def _get_opener(self, url):
         handler = urllib2.HTTPBasicAuthHandler()
         (scheme, netloc, path, params, query, fragment) = urlparse.urlparse(url)
-        handler.add_password("Restricted Access", netloc, self.username, self.password)
+
+        handler.add_password( "Restricted Access", 
+                              netloc,  self.username, self.password)
+
         opener = urllib2.build_opener(handler)
         return opener
 
     def get_file_url(self, filepath):
         url = self.url + "/json/get"
-        return self._fetch_url(url, path=filepath)
+        return self._fetch_json_url(url, path=filepath)
 
     def get_upload_url(self, filepath, base64_of_md5):
         url = self.url + "/json/put"
-        return self._fetch_url(url, path=filepath, base64_of_md5=base64_of_md5)
+        return self._fetch_json_url(url, path=filepath, 
+                                    base64_of_md5=base64_of_md5)
 
     def get_delete_url(self, filepath):
         url = self.url + "/json/delete"
-        return self._fetch_url(url, path=filepath)
+        return self._fetch_json_url(url, path=filepath)
 
-    def get_list_url(self):
+    def get_list_url(self, marker=None):
+
+        options = {}
+
+        if marker:
+            options["marker"] = marker
+
         url = self.url + "/json/list"
-        return self._fetch_url(url)
+        return self._fetch_json_url(url, **options)
 
-    def download_file(self, filename, dest, limitrate=None, callback=None, overwrite=False):
+    def download_file( self, filename, dest, 
+                       limitrate=None, callback=None, overwrite=False):
+
         url = self.get_file_url(filename)['url']
+        self._download_file( url, dest, limitrate, callback, overwrite ) 
 
+
+    def _download_file( self, url, dest, 
+                       limitrate=None, callback=None, overwrite=False):
         mode = "wb"
         resume = False
 
@@ -122,7 +155,7 @@ class Api(object):
         fp = _File(dest, mode, callback)
         curl = pycurl.Curl()
 
-        curl.setopt(pycurl.URL, url)
+        curl.setopt(pycurl.URL, str(url))
         curl.setopt(pycurl.CONNECTTIMEOUT, 30)
         curl.setopt(pycurl.TIMEOUT, 300)
         curl.setopt(pycurl.NOPROGRESS, 1)
@@ -150,7 +183,7 @@ class Api(object):
 
         url = self.get_delete_url(filename)['url']
         curl = pycurl.Curl()
-        curl.setopt(pycurl.URL, url)
+        curl.setopt(pycurl.URL, str(url))
         curl.setopt(pycurl.CUSTOMREQUEST, "DELETE")
         curl.setopt(pycurl.CONNECTTIMEOUT, 30)
         curl.setopt(pycurl.TIMEOUT, 300)
@@ -161,17 +194,20 @@ class Api(object):
 
 
 
-    def upload_file(self, filename, dest, limitrate=None, callback=None):
+    def upload_file(self, filename, dest, sent_md5=True,
+                    limitrate=None, callback=None):
 
-        filemd5 = _md5_for_file(filename)
-        base64_of_md5 = base64.standard_b64encode(filemd5)
+        if sent_md5:
+            filemd5 = _md5_for_file(filename)
+            base64_of_md5 = base64.standard_b64encode(filemd5)
+
         filereader = _File(filename, "rb", callback)
 
         url = self.get_upload_url(dest, base64_of_md5)['url']
 
         curl = pycurl.Curl()
 
-        curl.setopt(pycurl.URL, url)
+        curl.setopt(pycurl.URL, str(url))
         curl.setopt(pycurl.UPLOAD, 1)
         curl.setopt( pycurl.READFUNCTION, 
                      filereader.read )
@@ -180,7 +216,10 @@ class Api(object):
         curl.setopt(pycurl.NOPROGRESS, 1)
         curl.setopt(pycurl.CONNECTTIMEOUT, 30)
         curl.setopt(pycurl.TIMEOUT, 300)
-        curl.setopt(pycurl.HTTPHEADER, ["Content-MD5:" + base64_of_md5])
+
+        if sent_md5:
+            curl.setopt(pycurl.HTTPHEADER, ["Content-MD5:" + base64_of_md5])
+
         curl.setopt(pycurl.NOSIGNAL, 1)
 
         if callback:
@@ -192,6 +231,43 @@ class Api(object):
 
         curl.perform()
         curl.close()
+
+
+    def list_files(self, marker=None):
+        info = self.get_list_url(marker)
+        content = self._fetch_url(info['url'])
+        if info['service'] == 'amazon':
+            items = S3.ListBucketResponse( _S3HttpResponseFake(content))
+            truncated = items.is_truncated
+            return [ (e.key, e.size) for e in items.entries ], truncated
+
+
+    def list_all_files(self):
+        result = []
+        entries, is_truncated = self.list_files()
+        result.extend( entries )
+
+        while is_truncated:
+
+            retry = 0
+            
+            while retry < 3:
+                try:
+                    last = result[-1][0] # entries = [ (file, size) ]
+                    entries, is_truncated = self.list_files(marker=last)
+                    result.extend( entries )
+                    break
+                except urllib2.URLError, e:
+                    retry += 1
+
+        return result
+        
+
+
+    def get_size(self):
+        entries = self.list_all_files()
+        return sum( e[1] for e in entries )
+
 
 
 
