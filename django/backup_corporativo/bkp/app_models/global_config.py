bb00410026889c39c1a2f741465e71d182379910
Rodrigo Araújo <fenrrir@gmail.com>
2010-05-07 20:57:06 -0300

    Adicionado campos de configuração do offsite (user, password, url e rate limit)

--- django/backup_corporativo/bkp/app_models/global_config.py
+++ django/backup_corporativo/bkp/app_models/global_config.py
@@ -28,6 +28,11 @@ class GlobalConfig(models.Model):
     storage_port = models.IntegerField("Porta do Storage", default='9103')
     offsite_on = models.BooleanField("Offsite ativo?", default=False)
     offsite_hour = models.TimeField("Horário", default="00:00:00")
+    offsite_username = models.CharField(max_length=255,null=False,blank=False)
+    offsite_password = models.CharField(max_length=255,null=False,blank=False)
+    offsite_gateway_url = models.CharField(max_length=255,null=False,blank=False,
+                                 default="http://gateway.nimbus.com")
+    offsite_upload_rate = models.IntegerField(default=-1,null=False, blank=False)
     total_backup_size = models.FloatField("Tamanho Total do Backup (GB)")
 
     # Classe Meta é necessária para resolver um problema gerado quando se
