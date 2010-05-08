775544586bedf9f6448c27e429b4d94e325eaf66
Rodrigo Araújo <fenrrir@gmail.com>
2010-05-07 21:00:52 -0300

    Adicionando funções de alto nível para facilitar criação de views e import das views de offsite e storage adicional

--- django/backup_corporativo/bkp/views.py
+++ django/backup_corporativo/bkp/views.py
@@ -3,11 +3,12 @@
 
 import os
 
-from django.http import HttpResponse
-from django.http import HttpResponseRedirect
+from django.http import HttpResponse, Http404, HttpResponseRedirect
 from django.template import RequestContext
-from django.shortcuts import render_to_response
+from django.shortcuts import render_to_response as _render_to_response
 from django.shortcuts import get_object_or_404
+from django.views.generic import list_detail
+from django.views.generic.simple import  direct_to_template
 
 
 from backup_corporativo.bkp import utils
@@ -46,6 +47,20 @@ def global_vars(request):
     return vars_dict, forms_dict        
 
 
+
+def default_context_processor(request):
+    """ A context processor that provider request, script_name,
+        current_user and current_time """
+    return { 
+      "request" : request,
+      "script_name" : request.META['SCRIPT_NAME'],
+      "current_user" : request.user,
+      "current_time" : utils.current_time()
+    }
+
+
+
+
 def require_authentication(request):
     """Redirect user to authentication page."""
     return HttpResponseRedirect(utils.reverse('new_session'))
@@ -70,6 +85,65 @@ def authentication_required(view_def):
     return check_auth
 
 
+@authentication_required
+def restricted_object_list(*args, **kwargs):
+    return list_detail.object_list(*args, **kwargs)
+
+
+
+@authentication_required
+def restricted_object_detail(*args, **kwargs):
+    return list_detail.object_detail(*args, **kwargs)
+
+
+@authentication_required
+def restricted_direct_to_template(*args, **kwargs):
+    return direct_to_template(*args, **kwargs)
+
+
+def make_list_generic_view_dict(template, queryset):
+    return {
+        "template_name" : template,
+        "queryset" : queryset,
+        "paginate_by" : 25,
+        "context_processors" : [default_context_processor]}
+
+
+###
+### Templates
+###
+
+def render(request, template_name, dictionary):
+    return _render_to_response( template_name, dictionary,
+                                context_instance=RequestContext(request,
+                                processors = [default_context_processor]))
+
+
+
+def make_method_handler(method_name):
+
+    def handler(function):
+
+        @authentication_required
+        def wrapper(request, *args, **kwargs):
+            if request.method == method_name.upper():
+                return function(request, *args, **kwargs)
+            else:
+                raise Http404()
+
+        wrapper.__name__ = function.__name__
+        wrapper.__doc__ = function.__doc__
+        wrapper.__dict__ = function.__dict__
+
+        return wrapper
+
+    handler.__name__ = method_name
+    return handler
+
+
+allow_post = make_method_handler("post")
+allow_get = make_method_handler("get")
+
 # External views
 from backup_corporativo.bkp.app_views.management import *
 from backup_corporativo.bkp.app_views.system import *
@@ -80,5 +154,7 @@ from backup_corporativo.bkp.app_views.procedures import *
 from backup_corporativo.bkp.app_views.schedules import *
 from backup_corporativo.bkp.app_views.stats import *
 from backup_corporativo.bkp.app_views.tmp_restores import *
+from backup_corporativo.bkp.app_views.externalstorages import *
+from backup_corporativo.bkp.app_views.offsite import *
 
 from backup_corporativo.bkp.app_views.wizard import *
