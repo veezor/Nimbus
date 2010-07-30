3bcc8ee851307cb876e56ce344f3b195cbbc80b5
Rodrigo Araújo <fenrrir@gmail.com>
2010-07-30 18:50:55 -0300

    Adicionando middleware para configurar sistema de logging

--- nimbus/shared/middlewares.py
+++ nimbus/shared/middlewares.py
@@ -1,12 +1,27 @@
 #!/usr/bin/env python
 # -*- coding: UTF-8 -*-
 
+import os
+import sys
 import logging
 import traceback
+import logging.config
+
+from django.conf import settings
+
+
+
+class LogSetup(object): # on bootstrap
+
+    def __init__(self):
+        logging.config.fileConfig(settings.LOGGING_CONF)
+
+
+
 
 class AjaxDebug(object):
 
     def process_exception(self, request, exception):
-        traceback.print_exc()
+        traceback.print_exc(file=sys.stderr)
         logger = logging.getLogger(__name__)
         logger.exception("Exception levantada no django")
