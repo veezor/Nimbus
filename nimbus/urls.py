be6ae3133d63d125fc4a608fde014158c06d5bfc
Rodrigo Araújo <fenrrir@gmail.com>
2010-07-30 18:48:22 -0300

    Adicionando suporte a admin no urls

--- nimbus/urls.py
+++ nimbus/urls.py
@@ -2,12 +2,14 @@ from django.conf.urls.defaults import *
 from django.conf import settings
 
 # Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
+from django.contrib import admin
+
+if settings.DEBUG:
+    admin.autodiscover()
 
 urlpatterns = patterns('',
     (r'^base/', include('nimbus.base.urls')),
-    (r'^session/', include('nimbus.auth.urls')),
+    (r'^session/', include('nimbus.session.urls')),
     (r'^users/', include('nimbus.users.urls')),
     (r'^network/', include('nimbus.network.urls')),
     (r'^timezone/', include('nimbus.timezone.urls')),
@@ -19,4 +21,10 @@ if settings.SERVE_STATIC_FILES:
     urlpatterns += patterns('',
     (r'^media/(?P<path>.*)$', 'django.views.static.serve', 
         {'document_root': settings.MEDIA_ROOT}),  
+)
+
+
+if settings.DEBUG:
+    urlpatterns += patterns('',
+    ('^admin/', include(admin.site.urls)),
 )
