90723abb05701ee5326221f5541b70098df36508
Jonatas Oliveira <jonatas.oliveira@multmeio.com.br>
2010-10-05 15:41:51 -0300

    Adicionando a pagina de redirecionamento quando a configuracao eh atualizada.

--- nimbus/network/urls.py
+++ nimbus/network/urls.py
@@ -5,4 +5,5 @@ from django.conf.urls.defaults import *
 
 urlpatterns = patterns('nimbus.network.views',
     (r'^config/$', 'network_conf'), 
+    (r'^redirect_after_update/$', 'redirect_after_update'), 
 )
