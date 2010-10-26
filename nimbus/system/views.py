0efea74bda66cadde04f937e3fa3d241ad3c66a3
Rodrigo Araújo <fenrrir@gmail.com>
2010-10-26 14:16:37 -0300

    FIX: Correção no calculo de memória livre

--- nimbus/system/views.py
+++ nimbus/system/views.py
@@ -80,11 +80,13 @@ def create_or_view_network_tool(request):
 
 
 def stat(request):
+    memory = systeminfo.get_memory_usage()
+    memory_free = 100 - memory
     extra_content = { 
         'title': u"Estatística do sistema",
         'cpu' : systeminfo.get_cpu_usage(),
-        'memory' : systeminfo.get_memory_usage(),
-        'memory_free' : 100 - systeminfo.get_memory_usage()
+        'memory' : memory,
+        'memory_free' : memory_free
     }
 
     return render_to_response(request, "stat.html", extra_content)
