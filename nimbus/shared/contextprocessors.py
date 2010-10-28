3e0f901192c11c11fc7ef200711a0ed6dd1293b9
Jonatas Oliveira <jonatas.oliveira@multmeio.com.br>
2010-10-28 12:07:37 -0300

    O status do servidor no cabeçalho está funcionando dinamicamente através de um contextprocessor.

--- nimbus/shared/contextprocessors.py
+++ nimbus/shared/contextprocessors.py
@@ -6,6 +6,14 @@ def script_name(request):
     # return { 'script_name' : request.META['SCRIPT_NAME']} 
     return { 'script_name' : request.META['PATH_INFO']}
 
+
+def status_header(request):
+    from random import choice
+    d = [{'status_header': {'status_name': 'ok', 'status_message': 'Normal'}},
+         {'status_header': {'status_name': 'error', 'status_message': 'Erro'}}]
+    return choice(d)
+
+
 # Exemplo.
 # def computers(request):
 #     # return { 'script_name' : request.META['SCRIPT_NAME']} 
