54704bd6d04240f205417d23b75bfe6173755b5f
Rodrigo Araújo <fenrrir@gmail.com>
2010-11-01 15:25:00 -0300

    scripts para geração de executável

---
+++ nimbus/settings_executable.py
@@ -0,0 +1,201 @@
+# Django settings for nimbus project.
+
+from os.path import join, dirname
+
+DEBUG = False
+TEMPLATE_DEBUG = DEBUG
+AJAX_DEBUG = DEBUG
+
+ADMINS = (
+     ('Suporte Nimbus', 'nimbus.suporte@nimbus.veezor.com'),
+)
+
+MANAGERS = ADMINS
+
+
+DATABASES = {
+    'default': {
+        'NAME': 'nimbus',
+        'ENGINE' : 'django.db.backends.mysql',
+        'USER' : 'root',
+        'PASSWORD' : 'n1mbus',
+        'HOST' : 'localhost'
+    },
+    'bacula' : {
+        'NAME': 'bacula',
+        'ENGINE' : 'django.db.backends.mysql',
+        'USER' : 'root',
+        'PASSWORD' : 'n1mbus',
+        'HOST' : 'localhost'
+    }
+}
+
+
+### NIMBUS
+
+
+NIMBUS_MANAGER_URL = "http://localhost:8888"
+
+ROOT_PATH = "/"
+
+
+
+NIMBUS_ETC_DIR = join(ROOT_PATH, "etc", "nimbus")
+NIMBUS_HOME_DIR = join(ROOT_PATH, "var", "nimbus")
+
+NIMBUS_SSLCONFIG = join(NIMBUS_ETC_DIR, "ssl.conf")
+
+
+NIMBUS_DEPLOY_PATH = join(ROOT_PATH, "var","www")
+
+
+NIMBUS_CUSTOM_DIR = join(NIMBUS_HOME_DIR, "custom" )
+NIMBUS_CONFIG_DIR = join(NIMBUS_CUSTOM_DIR, "config" )
+NIMBUS_CERTIFICATES_DIR = join(NIMBUS_CUSTOM_DIR, "certificates" )
+NIMBUS_JOBS_DIR = join(NIMBUS_CUSTOM_DIR, "jobs" )
+NIMBUS_COMPUTERS_DIR = join(NIMBUS_CUSTOM_DIR, "computers" )
+NIMBUS_FILESETS_DIR = join(NIMBUS_CUSTOM_DIR, "filesets" )
+NIMBUS_POOLS_DIR = join(NIMBUS_CUSTOM_DIR, "pools" )
+NIMBUS_DEVICES_DIR = join(NIMBUS_CUSTOM_DIR, "devices" )
+NIMBUS_STORAGES_DIR = join(NIMBUS_CUSTOM_DIR, "storages" )
+NIMBUS_SCHEDULES_DIR = join(NIMBUS_CUSTOM_DIR, "schedules" )
+
+BCONSOLE_CONF = join(NIMBUS_CONFIG_DIR, "bconsole.conf")
+BACULADIR_CONF = join(NIMBUS_CONFIG_DIR, "bacula-dir.conf")
+BACULAFD_CONF = join(NIMBUS_CONFIG_DIR, "bacula-fd.conf")
+BACULASD_CONF = join(NIMBUS_CONFIG_DIR, "bacula-sd.conf")
+LOGGING_CONF = join(NIMBUS_ETC_DIR, "logging.conf")
+PYBACULA_TEST = True
+
+
+NIMBUS_UNDEPLOYED_CONF_FILES = join( dirname(__file__), 'confs')
+NIMBUS_UNDEPLOYED_LOG_CONF = join(NIMBUS_UNDEPLOYED_CONF_FILES, "logging.conf")
+
+RESTORE_POINT_DEFAULT = "/tmp/bacula-restore"
+
+NIMBUS_WIZARD = True
+
+# Nimbus log verbose
+LOG_DEBUG = True
+
+# Static files
+SERVE_STATIC_FILES = False
+
+
+NIMBUS_CLIENT_PORT = 11110
+# END NIMBUS
+
+# Local time zone for this installation. Choices can be found here:
+# http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
+# although not all choices may be available on all operating systems.
+# If running in a Windows environment this must be set to the same as your
+# system time zone.
+TIME_ZONE = ''
+
+# Language code for this installation. All choices can be found here:
+# http://www.i18nguy.com/unicode/language-identifiers.html
+LANGUAGE_CODE = 'pt-br'
+
+SITE_ID = 1
+
+# If you set this to False, Django will make some optimizations so as not
+# to load the internationalization machinery.
+USE_I18N = True
+
+# Absolute path to the directory that holds media.
+# Example: "/home/media/media.lawrence.com/"
+if SERVE_STATIC_FILES:
+    MEDIA_ROOT = join( dirname(__file__), 'media')
+else:
+    MEDIA_ROOT = join( NIMBUS_DEPLOY_PATH, 'media' )
+
+# URL that handles the media served from MEDIA_ROOT. Make sure to use a
+# trailing slash if there is a path component (optional in other cases).
+# Examples: "http://media.lawrence.com", "http://example.com/media/"
+MEDIA_URL = ''
+
+# URL prefix for admin media -- CSS, JavaScript and images. Make sure to use a
+# trailing slash.
+# Examples: "http://foo.com/media/", "/media/".
+ADMIN_MEDIA_PREFIX = '/adminmedia/'
+
+# Make this unique, and don't share it with anybody.
+SECRET_KEY = '7qj040(6uwuuzx+y&ety-bs5i$*q$0tag02q+sx8th_%1w$h9%'
+
+
+LOGIN_REDIRECT_URL = "/"
+LOGIN_URL = "/session/login"
+
+
+TEMPLATE_CONTEXT_PROCESSORS = (
+    "nimbus.shared.contextprocessors.script_name",
+    "django.core.context_processors.auth",
+    "django.core.context_processors.debug",
+    "django.core.context_processors.i18n",
+    "django.core.context_processors.media" ,
+    "django.contrib.messages.context_processors.messages"
+)
+
+# List of callables that know how to import templates from various sources.
+TEMPLATE_LOADERS = (
+    'django.template.loaders.filesystem.load_template_source',
+    'django.template.loaders.app_directories.load_template_source',
+#     'django.template.loaders.eggs.load_template_source',
+)
+
+MIDDLEWARE_CLASSES = (
+    'nimbus.shared.middlewares.LogSetup',
+    'nimbus.shared.middlewares.ThreadPool',
+    'django.middleware.common.CommonMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+
+)
+
+
+DATABASE_ROUTERS = ['nimbus.bacula.dbrouting.Router']
+
+if AJAX_DEBUG:
+    MIDDLEWARE_CLASSES += ('nimbus.shared.middlewares.AjaxDebug' ,)
+
+
+if NIMBUS_WIZARD:
+    MIDDLEWARE_CLASSES += ('nimbus.wizard.middleware.Wizard' ,)
+
+
+ROOT_URLCONF = 'nimbus.urls'
+
+TEMPLATE_DIRS = (
+    # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
+    # Always use forward slashes, even on Windows.
+    # Don't forget to use absolute paths, not relative paths.
+)
+
+INSTALLED_APPS = (
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.sites',
+    'nimbus.bacula',
+    'nimbus.session',
+    'nimbus.base',
+    'nimbus.users',
+    'nimbus.config',
+    'nimbus.network',
+    'nimbus.schedules',
+    'nimbus.filesets',
+    'nimbus.storages',
+    'nimbus.pools',
+    'nimbus.procedures',
+    'nimbus.timezone',
+    'nimbus.offsite',
+    'nimbus.wizard',
+    'nimbus.computers',
+    'nimbus.backup',
+    'nimbus.restore',
+    'nimbus.system'
+)
+
+if DEBUG:
+    INSTALLED_APPS += ("django.contrib.admin",)
--- scripts/generate_deb.sh
+++ scripts/generate_deb.sh
@@ -10,14 +10,18 @@ source initenv.sh;
 makedir "deb/var"
 makedir "deb/etc/init.d"
 makedir "deb/etc/nimbus"
-makedir "deb/etc/apache2/sites-enabled"
+makedir "deb/etc/nginx/sites-available"
+makedir "deb/etc/nginx/sites-enabled"
 makedir "deb/var/log/nimbus"
 makedir "deb/var/nimbus/deps"
+makedir "deb/var/nimbus/custom"
+
 
 find deb -iname "*~" -exec rm {} \;
 
-cd django
-cp backup_corporativo/settings_executable.py backup_corporativo/settings.py;
+
+cd nimbus
+cp settings_executable.py settings.py;
 python setup.py build_exe ;
 cp -a binary ../deb/var/www;
 cd ..;
@@ -29,16 +33,15 @@ cp -a binary ../../deb/var/nimbusmanager;
 cd ../..;
 
 
-cp django/apacheconf/default deb/etc/apache2/sites-enabled/000-default
-cp -a django/cron.daily deb/etc/
-cp -a custom deb/var/nimbus
-cp django/backup_corporativo/logging.conf deb/etc/nimbus
+cp nimbus/confs/nginx-nimbus.site deb/etc/nginx/sites-enabled/default
+cp nimbus/confs/nimbus/cron deb/etc/cron.daily
+cp nimbus/confs/logging.conf deb/etc/nimbus
+cp nimbus/confs/nimbus.initd deb/etc/init.d
 cp webservices/manager/nimbus_manager.conf deb/etc/nimbus
 cp libs/keymanager/conf/ssl.conf deb/etc/nimbus
 cp webservices/manager/init.d/nimbusmanager deb/etc/init.d
 
 dpkg-deb -b deb nimbus.deb
-
 
 rm -rf deb/var/www
 rm -rf deb/var/nimbus/custom
