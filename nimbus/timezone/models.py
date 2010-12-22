3711b72337f7ce7012d0d014d0bfc927d5753571
Rodrigo Araújo <fenrrir@gmail.com>
2010-12-22 15:09:03 -0300

    FIX: sanitização do ntp_server

--- nimbus/timezone/models.py
+++ nimbus/timezone/models.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: UTF-8 -*-
 
-
+import re
 import logging
 import time
 from operator import itemgetter
@@ -13,11 +13,15 @@ from pytz import country_names
 from django.db import models
 from django.db.models.signals import post_save
 from django.conf import settings
+from django.core.exceptions import ValidationError
+
 
 from nimbus.shared import signals
 from nimbus.shared.middlewares import ThreadPool
 from nimbus.base.models import UUIDSingletonModel as BaseModel
 
+
+DOMAIN_RE = re.compile(r"^(\w+\.)?\w+\.\w+$")
 
 EMPTY_CHOICES = [('', '----------')]
 
@@ -26,14 +30,21 @@ COUNTRY_CHOICES = [ item \
                     sorted(country_names.items(), key=itemgetter(1)) ]
 
 
+def check_domain(value):
+    if not DOMAIN_RE.match(value):
+        raise ValidationError("ntp_server must be a domain")
+
+
 
 class Timezone(BaseModel):
     ntp_server = models.CharField( max_length=255, blank=False,
-                                   null=False, default="a.ntp.br")
+                                   null=False, default="a.ntp.br",
+                                   validators=[check_domain])
     country = models.CharField( max_length=255, blank=False, 
                                 choices=COUNTRY_CHOICES)
     area = models.CharField( max_length=255, blank=False, 
                              null=False)
+
 
 
 
