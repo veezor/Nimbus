ab662b1282ce3c5fd277cbf0fb82ef0627bc2ffa
Rodrigo Araújo <fenrrir@gmail.com>
2011-01-11 10:38:41 -0300

    FIX: Corrigindo dados sobre procedimentos do storage

--- nimbus/storages/views.py
+++ nimbus/storages/views.py
@@ -4,12 +4,15 @@
 
 # Create your views here.
 
+import systeminfo
+
 from django.http import Http404, HttpResponse
 from django.views.generic import create_update
 from django.core.urlresolvers import reverse
 from django.shortcuts import redirect
 from django.db import IntegrityError
 from django.contrib.auth.decorators import login_required
+from django.contrib import messages
 
 from nimbus.storages.models import Storage
 from nimbus.storages.models import Device
@@ -17,6 +20,7 @@ from nimbus.storages.forms import StorageForm
 from nimbus.computers.models import Computer
 from nimbus.shared.views import render_to_response
 from nimbus.shared.forms import form
+from nimbus.bacula.models import Job
 
 
 # def add(request):
@@ -88,20 +92,43 @@ def list(request):
 @login_required
 def view(request, object_id):
     storage = Storage.objects.get(id=object_id)
+
+    running_status = ('R','p','j','c','d','s','M','m','s','F','B')
+    running_jobs = Job.objects.filter( jobstatus__in=running_status)\
+                                 .order_by('-starttime').distinct()[:5]
+
+
+    running_procedures_content = []
+    try:
+        for job in running_jobs:
+            running_procedures_content.append({
+                'type' : 'ok',
+                'label' : job.procedure.name,
+                'date' : job.starttime,
+                'message' : u'Computador : %s' % job.client.computer.name
+            })
+    except (Procedure.DoesNotExist, Computer.DoesNotExist), error:
+        pass
+
+
     backups_em_execucao = [{
         'title': u'Backups em Execução',
-        'content': [{
-            'type' : 'ok',
-            'label' : 'aaa',
-            'date' : '20/12/2010 10:00',
-            'message' : u'Computador : %s' % 'Computer'
-    }]}]
+        'content': running_procedures_content
+    }]
+
+    diskinfo = systeminfo.DiskInfo("/bacula")
+    try:
+        diskusage = diskinfo.get_usage()
+    except OSError, error:
+        messages.error(request, "Partição /bacula não encontrada")
+        diskusage = 0
+
+
     d = {
         "storage" : storage,
         "title": u"Armazenamento",
         "backups_em_execucao": backups_em_execucao,
-        # TODO: Isso não é para ficar fixo.
-        "espaco_em_disco": '70'
+        "espaco_em_disco": diskusage
     }
     
     return render_to_response(request, "storages_view.html", d)
