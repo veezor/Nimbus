6495931d25338a067153de0d9bca4433b03eac2f
Karlisson M. Bezerra <karlisson.bezerra@veezor.com.br>
2011-02-03 21:42:22 -0300

    Modifiquei a view para realizar a filtragem por grupo quando uma requisicao GET eh enviada.

--- nimbus/computers/views.py
+++ nimbus/computers/views.py
@@ -108,7 +108,11 @@ def delete(request, object_id):
 
 @login_required
 def list(request):
-    computers = Computer.objects.filter(active=True).order_by('groups__name')
+    if request.method == "GET":
+        group = request.GET.get("group")
+        computers = Computer.objects.filter(active=True, groups__name=group).order_by('groups__name')
+    else:
+        computers = Computer.objects.filter(active=True).order_by('groups__name')
     groups = ComputerGroup.objects.order_by('name')
     extra_content = {
         'computers': computers,
