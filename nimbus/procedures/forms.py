25df33eb87fa4674ed5f8b54b000771049f5ca67
Rodrigo Araújo <rodrigo.araujo@veezor.com>
2011-02-09 11:40:59 -0300

    Revertendo refatoração de Oliveira

--- nimbus/procedures/forms.py
+++ nimbus/procedures/forms.py
@@ -12,9 +12,3 @@ class ProfileForm(forms.ModelForm):
             'schedule': forms.widgets.Select(attrs={'class': 'styled'}),
             'fileset': forms.widgets.Select(attrs={'class': 'styled'}),
         }
-
-
-class ProcedureForm(forms.ModelForm):
-    class Meta:
-        model = Procedure
-
