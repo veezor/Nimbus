4d8426158c4f2adfa38c1e6071b8cf5f518d06d4
Rodrigo Araújo <rodrigo.araujo@veezor.com>
2011-02-09 08:28:27 -0300

    FIX: corrigindo o initenv.sh para o novo nimbus

--- initenv.sh
+++ initenv.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # copiando setting.py
-if [ ! -f django/backup_corporativo/settings.py ];
+if [ ! -f nimbus/settings.py ];
 then
     echo "copiando settings.py"
-    cp -a django/backup_corporativo/settings_sample.py django/backup_corporativo/settings.py;
+    cp -a nimbus/settings_sample.py nimbus/settings.py;
 fi
 
 # sobrescrevendo PYTHONPATH
