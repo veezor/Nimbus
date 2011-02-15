496597a61a3b9967e76d6bcb45838a778e331789
Karlisson M. Bezerra <karlisson.bezerra@veezor.com.br>
2011-02-15 10:54:16 -0300

    Organizando o codigo da app system.

---
+++ nimbus/media/js/system.js
@@ -0,0 +1,52 @@
+$(document).ready(function(){
+
+    function update_table(table) {
+        table = $('.request_list');
+        $.post($(".atualizar_agora").attr("rel"), {ajax: 1}, function(data)
+        {
+            table.find('tbody tr').remove();
+            for (var item in data) {
+                down = data[item];
+                tr = $('<tr>');
+                pid = $('<td>').text(down.fields.pid);
+                name = $('<td>').text(down.fields.name);
+                criado_em = $('<td>').text(down.fields.created_at);
+                estado = $('<td>').text(down.fields.status);
+                
+                tr.append(pid).append(name).append(criado_em).append(estado);
+                tr.appendTo(table.find('tbody'));
+            }
+        },
+        "json");
+    }
+    
+    var countDownInterval = 20;
+    var countDownTime = countDownInterval + 1;
+    var counter = undefined;
+    
+    function countDown(){
+        countDownTime--;
+        if (countDownTime <= 0){
+            $('.tempo_restante').text(countDownTime);
+            countDownTime = countDownInterval;
+            clearInterval(counter);
+            update_table();
+            return startit();
+        }
+        $('.tempo_restante').text(countDownTime);
+    }
+
+    function startit(){
+        $('.tempo_restante').text(countDownTime);
+        countDown();
+        counter=setInterval(countDown, 1000);
+    }
+
+    startit();
+    $('.atualizar_agora').click(function(){
+        update_table();
+        countDownTime = countDownInterval;
+        clearInterval(counter);
+        startit();
+    });
+});
