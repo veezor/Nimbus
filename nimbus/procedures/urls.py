420299e41d00ef180312b691906a9d333f9479d9
Cicero Monteiro <cicero@cicero-Inspiron-1525.(none)>
2011-05-09 10:45:07 -0300

    FIX: Removida a quebra de layout no menu, devido ao atributo "z-index" do estilo "select.styled".

--- nimbus/procedures/urls.py
+++ nimbus/procedures/urls.py
@@ -6,7 +6,7 @@ from django.conf.urls.defaults import *
 urlpatterns = patterns('nimbus.procedures.views',
     (r'^$', 'list'), 
     (r'^list/$', 'list'), 
-    (r'^add/$', 'add'), 
+    (r'^add/$', 'add'),
     # (r'^(?P<object_id>\d+)/view/$', 'view'), 
     (r'^(?P<object_id>\d+)/edit/$', 'edit'),
     (r'^(?P<object_id>\d+)/delete/$', 'delete'),
--- nimbus/schedules/urls.py
+++ nimbus/schedules/urls.py
@@ -1,15 +1,31 @@
 #!/usr/bin/env python
 # -*- coding: UTF-8 -*-
 
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+
 from django.conf.urls.defaults import *
 
 urlpatterns = patterns('nimbus.schedules.views',
                         (r'^add/(?P<object_id>\d+)$', 'render'),
-                        (r'^add/$', 'render'),
+                        (r'^add/$', 'add'),
                         (r'^profiles/add$', 'profile_new'),
                         (r'^schedules/add$', 'schedule_new'),
                         (r'^filesets/add/(?P<object_id>\d+)$', 'fileset_new'),
                         (r'^get_tree/$', 'get_tree'),
-    # (r'^add/$', 'add'), 
-    # (r'^(?P<object_id>\d+)/edit/$', 'edit'), 
+    # (r'^add/$', 'add'),
+                        (r'^(?P<object_id>\d+)/edit/$', 'edit'),
 )
+
+#from django.conf.urls.defaults import *
+#
+#urlpatterns = patterns('nimbus.schedules.views',
+#                        #(r'^add/(?P<object_id>\d+)$', 'render'),
+#                        #(r'^add/$', 'render'),
+#                        (r'^profiles/add$', 'profile_new'),
+#                        (r'^schedules/add$', 'schedule_new'),
+#                        (r'^filesets/add/(?P<object_id>\d+)$', 'fileset_new'),
+#                        (r'^get_tree/$', 'get_tree'),
+#                        (r'^(?P<object_id>\d+)/edit/$', 'edit'),
+#                        (r'^add/$', 'add')
+#)
