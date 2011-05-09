6385c7eeae2a12b75a8809a59d74ee4d80f30782
Fabio <fabio.pacheco@veezor.com>
2011-05-09 13:37:09 -0300

    Formatando 'computers' segundo PEP8

--- nimbus/computers/models.py
+++ nimbus/computers/models.py
@@ -3,6 +3,7 @@
 
 from os import path
 import xmlrpclib
+import keymanager
 
 from django.db import models
 from django.conf import settings
@@ -14,17 +15,16 @@ from nimbus.config.models import Config
 from nimbus.shared import utils, enums, signals, fields
 from nimbus.libs.template import render_to_file, render_to_string
 
-
-import keymanager
-
-
 OS = ( (os,os) for os in enums.operating_systems)
 
 
 class UnableToGetFile(Exception):
+    # TODO: TRATAR
     pass
 
+
 class ComputerAlreadyActive(Exception):
+    # TODO: TRATAR
     pass
 
 
@@ -37,19 +37,25 @@ class CryptoInfo(models.Model):
     certificate = models.CharField( max_length=2048, blank=False, null=False)
     pem = models.CharField( max_length=4096, blank=False, null=False)
 
+
 class ComputerNewClass(BaseModel):
-    name = models.CharField(max_length=255, unique=True, blank=False, null=False, validators=[fields.check_model_name])
+    name = models.CharField(max_length=255, unique=True, blank=False, null=False,
+                            validators=[fields.check_model_name])
 
 class Computer(BaseModel):
-    name = models.CharField(max_length=255, unique=True, blank=False, null=False, validators=[fields.check_model_name])
+    name = models.CharField(max_length=255, unique=True, blank=False, null=False,
+                            validators=[fields.check_model_name])
     address = models.IPAddressField(blank=False, null=False, unique=True)
-    operation_system = models.CharField(max_length=255, blank=False, null=False, choices=OS )
+    operation_system = models.CharField(max_length=255, blank=False, null=False,
+                                        choices=OS )
     description = models.TextField(max_length=1024, blank=True)
-    password = models.CharField(max_length=255, blank=False, null=False, editable=False, default=utils.random_password)
-    groups = models.ManyToManyField(ComputerGroup, related_name="computers", blank=True, null=True)
+    password = models.CharField(max_length=255, blank=False, null=False, 
+                                editable=False, default=utils.random_password)
+    groups = models.ManyToManyField(ComputerGroup, related_name="computers", 
+                                    blank=True, null=True)
     active = models.BooleanField(editable=False)
-    crypto_info = models.ForeignKey(CryptoInfo, null=False, blank=False, unique=True, editable=False)
-
+    crypto_info = models.ForeignKey(CryptoInfo, null=False, blank=False, 
+                                    unique=True, editable=False)
 
     def _get_crypt_file(self, filename):
         km = KeyManager()
@@ -63,10 +69,8 @@ class Computer(BaseModel):
         except IOError, e:
             raise UnableToGetFile("Original error was: %s" % e)      
 
-
     def get_pem(self):
         return self._get_crypt_file("client.pem")
-
 
     def get_config_file(self):
         config = Config.get_instance()
@@ -77,123 +81,87 @@ class Computer(BaseModel):
                                 os=self.operating_systems,
                                 nimbus=False)
 
-
     @property
     def bacula_id(self):
         return Client.objects.get(name=self.bacula_name).clientid
 
     def successful_jobs(self):
-        return Job.objects.filter( jobstatus__in=('T','W'), 
-                             client__name=self.bacula_name)\
-                                     .order_by('-endtime').distinct()[:15]
+        return Job.objects.filter(jobstatus__in=('T','W'), 
+                                  client__name=self.bacula_name)\
+                                        .order_by('-endtime').distinct()[:15]
 
     def unsuccessful_jobs(self):
-        return Job.objects.filter( jobstatus__in=('E','e','f','I'), 
-                             client__name=self.bacula_name)\
-                                     .order_by('-endtime').distinct()[:15]
+        return Job.objects.filter(jobstatus__in=('E','e','f','I'), 
+                                  client__name=self.bacula_name)\
+                                        .order_by('-endtime').distinct()[:15]
 
     def running_jobs(self):
         status = ('R','p','j','c','d','s','M','m','s','F','B')
-        return Job.objects.filter( jobstatus__in=status, 
-                             client__name=self.bacula_name)\
-                                     .order_by('-starttime').distinct()[:5]
-
+        return Job.objects.filter(jobstatus__in=status, 
+                                  client__name=self.bacula_name)\
+                                        .order_by('-starttime').distinct()[:5]
 
     def last_jobs(self):
         return Job.objects.filter(client__name=self.bacula_name)\
-                                .order_by('-endtime').distinct()[:15]
-
+                                        .order_by('-endtime').distinct()[:15]
 
     def configure(self):
-
         nimbuscomputer = Computer.objects.get(id=1)
-
         url = "http://%s:%d" % (self.address, settings.NIMBUS_CLIENT_PORT)
         proxy = xmlrpclib.ServerProxy(url)
-        proxy.save_keys( self.crypto_info.pem, 
-                         nimbuscomputer.crypto_info.certificate)
-
+        proxy.save_keys(self.crypto_info.pem, 
+                        nimbuscomputer.crypto_info.certificate)
         config = Config.get_instance()
-        fdconfig = render_to_string( "bacula-fd",
-                        director_name=config.director_name,
-                        password=self.password,
-                        name=self.name,
-                        os=self.operation_system)
-
-        proxy.save_config( unicode(fdconfig) )
+        fdconfig = render_to_string("bacula-fd",
+                                    director_name=config.director_name,
+                                    password=self.password,
+                                    name=self.name,
+                                    os=self.operation_system)
+        proxy.save_config(unicode(fdconfig))
         proxy.restart_bacula()
 
-
-
-
     def activate(self):
-
         if self.active:
             raise ComputerAlreadyActive("Computer already active")
-
         self.configure() 
         self.active = True
         self.save()
 
-
     def get_file_tree(self, path):
-
         url = "http://%s:%d" % (self.address, settings.NIMBUS_CLIENT_PORT)
         proxy = xmlrpclib.ServerProxy(url)
-
         if self.operation_system == "windows" and path == "/":
             files = proxy.get_available_drives()
             files = [ fname[:-1] + '/' for fname in files ]
         else:
             files = proxy.list_dir(path)
-
         files.sort()
-
         return files
-
 
     def deactivate(self):
         self.active = False
         self.save()
 
-
     def __unicode__(self):
         return "%s (%s)" % (self.name, self.address)
 
 
-
 def update_computer_file(computer):
     """Computer update file"""
-
-
     if computer.active:
-
         name = computer.bacula_name
-
-
-        filename = path.join( settings.NIMBUS_COMPUTERS_DIR, 
-                              name)
-
-        render_to_file( filename,
-                        "client",
-                        name=name,
-                        ip=computer.address,
-                        password=computer.password)
-
-
+        filename = path.join( settings.NIMBUS_COMPUTERS_DIR, name)
+        render_to_file(filename, "client", name=name, ip=computer.address,
+                       password=computer.password)
 
 def remove_computer_file(computer):
     """Computer remove file"""
-
     if computer.active:
-
-        filename = path.join( settings.NIMBUS_COMPUTERS_DIR, 
-                              computer.bacula_name)
+        filename = path.join(settings.NIMBUS_COMPUTERS_DIR, 
+                             computer.bacula_name)
         utils.remove_or_leave(filename)
 
-
 def generate_keys(computer):
-
     try:
         computer.crypto_info
     except CryptoInfo.DoesNotExist, error:
@@ -201,8 +169,6 @@ def generate_keys(computer):
         info = CryptoInfo.objects.create(key=key, certificate=cert, pem=pem)
         computer.crypto_info = info
 
-
-
-signals.connect_on( generate_keys, Computer, pre_save)
-signals.connect_on( update_computer_file, Computer, post_save)
-signals.connect_on( remove_computer_file, Computer, post_delete)
+signals.connect_on(generate_keys, Computer, pre_save)
+signals.connect_on(update_computer_file, Computer, post_save)
+signals.connect_on(remove_computer_file, Computer, post_delete)
--- nimbus/computers/views.py
+++ nimbus/computers/views.py
@@ -14,71 +14,59 @@ from django.core import serializers
 from django.shortcuts import redirect
 from django.contrib import messages
 from django.db import IntegrityError
+from django.shortcuts import render_to_response as render_this
 
 from nimbus.computers.models import Computer, ComputerGroup, ComputerAlreadyActive
 from nimbus.procedures.models import Procedure
 from nimbus.bacula.models import Job
 from nimbus.shared.views import render_to_response
-from django.shortcuts import render_to_response as render_this
 from nimbus.shared import enums
 from nimbus.shared.forms import form
-
 from nimbus.computers import forms as forms
 
 @login_required
 def add(request):
     lforms = [ forms.ComputerForm ]
-
-    content = {
-        'title':u'Adicionar Computador',
-        'forms':lforms,
-        'computers':Computer.objects.filter(active=False)
-    }
-
+    content = {'title':u'Adicionar Computador',
+               'forms':lforms,
+               'computers':Computer.objects.filter(active=False)
+              }
     return render_this("computers_add.html", content)
 
 @login_required
 def edit_no_active(request, object_id):
     extra_context = {'title': u"Editar computador"}
     messages.warning(request, u"O computador ainda não foi ativado.")
-
-    return create_update.update_object( request,
-                                        object_id = object_id,
-                                        model = Computer,
-                                        #form_class = form(Computer),
-                                        form_class = forms.ComputerForm,
-                                        template_name = "base_computers.html",
-                                        extra_context = extra_context,
-                                        post_save_redirect = reverse("nimbus.computers.views.add"))
+    return create_update.update_object(request,
+                                       object_id = object_id,
+                                       model = Computer,
+                                       #form_class = form(Computer),
+                                       form_class = forms.ComputerForm,
+                                       template_name = "base_computers.html",
+                                       extra_context = extra_context,
+                                       post_save_redirect = reverse("nimbus.computers.views.add"))
 
 @login_required
 def new(request):
-
     if request.method == "POST":
         logger = logging.getLogger(__name__)
         try:
             os = request.POST['os']
             if os not in enums.operating_systems:
                 return HttpResponse(status=400)
-            
             name = request.META.get('REMOTE_HOST')
             if not name:
                 name = u"Adicionado automaticamente %s" % uuid.uuid4().hex
-
-
             address = request.META['REMOTE_ADDR']
-
             if Computer.objects.filter(address=address).count():
                 logger.error("Já existe computador com esse ip")
                 return HttpResponse(status=400)
-
             computer = Computer(name = name,
-                                 address = address,
-                                 operation_system=os,
-                                 description="Computador identificado automaticamente")
+                                address = address,
+                                operation_system=os,
+                                description="Computador identificado automaticamente")
             computer.save()
             logger.info("Computador adicionado com sucesso")
-
             return HttpResponse(status=200)
         except (KeyError, IntegrityError), e:
             logger.exception("Erro ao adicionar o computador")
@@ -87,15 +75,13 @@ def new(request):
 @login_required
 def edit(request, object_id):
     extra_context = {'title': u"Editar computador"}
-    return create_update.update_object( request, 
-                                        object_id = object_id,
-                                        model = Computer,
-                                        form_class = form(Computer),
-                                        template_name = "base_computers.html",
-                                        extra_context = extra_context,
-                                        post_save_redirect = "/computers/")
-
-
+    return create_update.update_object(request, 
+                                       object_id = object_id,
+                                       model = Computer,
+                                       form_class = form(Computer),
+                                       template_name = "base_computers.html",
+                                       extra_context = extra_context,
+                                       post_save_redirect = "/computers/")
 
 @login_required
 def delete(request, object_id):
@@ -109,8 +95,6 @@ def delete(request, object_id):
         remove_name = computer.name
         return render_to_response(request, 'remove.html', locals())
 
-
-
 @login_required
 def list(request):
     if request.method == "GET":
@@ -119,99 +103,72 @@ def list(request):
     else:
         computers = Computer.objects.filter(active=True).order_by('groups__name')
     groups = ComputerGroup.objects.order_by('name')
-    extra_content = {
-        'computers': computers,
-        'title': u"Computadores Ativos",
-        'groups': groups
-    }
+    extra_content = {'computers': computers,
+                     'title': u"Computadores Ativos",
+                     'groups': groups}
     return render_to_response(request, "computers_list.html", extra_content)
-
 
 @login_required
 def view(request, object_id):
     computer = Computer.objects.get(id=object_id,id__gt=1)
-
     running_status = ('R','p','j','c','d','s','M','m','s','F','B')
-    running_jobs = Job.objects.filter( jobstatus__in=running_status,
-                                       client__name=computer.bacula_name)\
-                                            .order_by('-starttime').distinct()[:5]
-
-
+    running_jobs = Job.objects.filter(jobstatus__in=running_status,
+                                      client__name=computer.bacula_name)\
+                                          .order_by('-starttime').distinct()[:5]
     running_procedures_content = []
     try:
         for job in running_jobs:
             running_procedures_content.append({
-                'type' : 'ok',
-                'label' : job.procedure.name,
-                'date' : job.starttime,
-                'tooltip' : job.status_message,
-                'message' : u'Computador : %s' % job.client.computer.name
-            })
+                    'type' : 'ok',
+                    'label' : job.procedure.name,
+                    'date' : job.starttime,
+                    'tooltip' : job.status_message,
+                    'message' : u'Computador : %s' % job.client.computer.name
+                    })
     except (Procedure.DoesNotExist, Computer.DoesNotExist), error:
         pass
 
-
-
     last_jobs = Job.objects.filter(client__name=computer.bacula_name)\
-                    .order_by('-endtime').distinct()[:5]
-
-
+                                            .order_by('-endtime').distinct()[:5]
     last_procedures_content = []
     try:
         for job in last_jobs:
             last_procedures_content.append({
-                'type' : job.status_friendly,
-                'label' : job.procedure.name,
-                'date' : job.endtime,
-                'tooltip' : job.status_message,
-                'message' : u'Computador : %s' % job.client.computer.name
-            })
+                    'type' : job.status_friendly,
+                    'label' : job.procedure.name,
+                    'date' : job.endtime,
+                    'tooltip' : job.status_message,
+                    'message' : u'Computador : %s' % job.client.computer.name
+                    })
     except (Procedure.DoesNotExist, Computer.DoesNotExist), error:
         pass
-
-
     errors_jobs = Job.objects.filter(jobstatus__in=('e','E','f'),
                                      client__name=computer.bacula_name)\
-                                    .order_by('-endtime').distinct()[:5]
-
-
+                                            .order_by('-endtime').distinct()[:5]
     errors_procedures_content = []
     try:
         for job in errors_jobs:
             errors_procedures_content.append({
-                'type' : job.status_friendly,
-                'label' : job.procedure.name,
-                'date' : job.endtime,
-                'tooltip' : job.status_message,
-                'message' : u'Computador : %s' % job.client.computer.name
-            })
+                    'type' : job.status_friendly,
+                    'label' : job.procedure.name,
+                    'date' : job.endtime,
+                    'tooltip' : job.status_message,
+                    'message' : u'Computador : %s' % job.client.computer.name
+                    })
     except (Procedure.DoesNotExist, Computer.DoesNotExist), error:
         pass
-
-
-  
-    backups_em_execucao = [{
-        'title': u'Backups em Execução',
-        'content': running_procedures_content 
-    }]
-
-
-    backups_com_falhas = [{
-        'title': u'Últimos backups executados',
-        'content': last_procedures_content  }, {
-        'title': u'Backups com falha',
-        'content': errors_procedures_content   
-    }]
- 
-    
-    extra_content = {
-        'computer': computer,
-        'title': u"Visualizar computador",
-        'backups_em_execucao': backups_em_execucao,
-        'backups_executados_e_com_falhas': backups_com_falhas,
-    }
+    backups_em_execucao = [{'title': u'Backups em Execução',
+                            'content': running_procedures_content}]
+    backups_com_falhas = [{'title': u'Últimos backups executados',
+                           'content': last_procedures_content  },
+                          {'title': u'Backups com falha',
+                           'content': errors_procedures_content}]
+    extra_content = {'computer': computer,
+                     'title': u"Visualizar computador",
+                     'backups_em_execucao': backups_em_execucao,
+                     'backups_executados_e_com_falhas': backups_com_falhas,
+                     }
     return render_to_response(request, "computers_view.html", extra_content)
-
 
 @login_required
 def group_add(request):
@@ -228,19 +185,14 @@ def group_add(request):
         response = dict(message='success')
     return HttpResponse(simplejson.dumps(response))
 
-
 @login_required
 def group_list(request):
     ajax = request.POST['ajax']
-        
     if not ajax:
         return redirect('/')
-
     groups = ComputerGroup.objects.all()
-    
     response = serializers.serialize("json", groups)
     return HttpResponse(response, mimetype="text/plain")
-
 
 @login_required
 def activate(request, object_id):
@@ -256,11 +208,8 @@ def activate(request, object_id):
     except (socket.error, xmlrpclib.Fault), error:
         messages.error(request, u'Impossível ativar computador, verifique a conexão')
         return redirect('nimbus.computers.views.add')
-
     messages.success(request, u'Computador ativado com sucesso.')
     return redirect('nimbus.computers.views.list')
-
-
 
 @login_required
 def deactivate(request, object_id):
@@ -270,8 +219,5 @@ def deactivate(request, object_id):
     except Computer.DoesNotExist, error:
         messages.error(request, u'Impossível desativar computador, computador inexistente')
         return redirect('nimbus.computers.views.list')
-
-
     messages.success(u'Computador desativado com sucesso.')
     return redirect('nimbus.computers.views.list')
-
