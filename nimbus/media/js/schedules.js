ea7e8744bc92553969a5cd62adef9a8ccf638ec3
Cicero Monteiro <cicero.monteiro@veezor.com>
2011-05-23 11:34:27 -0300

    Exibição de vários erros no agendamento do backup.

--- nimbus/media/js/schedules.js
+++ nimbus/media/js/schedules.js
@@ -21,36 +21,16 @@ $(document).ready(function(){
         alert (dataString);
         $.ajax({
             type: "POST",
-            url: "/backup/schedules/add",
+            url: "/schedules/add",
             data: dataString,
             success: function() {
                 alert("Cadastro efetuado com sucesso!");
-                $(document).trigger('close.facebox');
+                //$(document).trigger('close.facebox');
             },
             error: function(){
                 alert('Houve um problema com a requisição');
-                $(document).trigger('close.facebox');
+                //$(document).trigger('close.facebox');
             }
         });
     });
 });
-    // colorbox scripts
-//    $(".schedule").colorbox({
-//        href: "/backup/schedules/add",
-//        scrolling:false
-//    });
-//    $(".fileset").click(function(){
-//        //$.colorbox.close();
-//        if ($("#id_procedure-computer").val())
-//        {
-//            $.colorbox({
-//                // href: "/backup/filesets/add/"+$("#id_procedure-computer").val()
-//                href: "/filesets/add/"+$("#id_procedure-computer").val()
-//            });
-//        }
-//        else
-//        {
-//            $.colorbox({html:"<h2>Atenção!</h2><h3>Você precisa escolher um computador</h3>"});
-//        }
-//        return false;
-//    });
