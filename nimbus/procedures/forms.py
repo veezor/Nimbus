592d24309b3027f2f241857fb5558befb1a928cc
Fabio <fabio.pacheco@veezor.com>
2011-06-01 12:30:43 -0300

    Formulário de edição de procedures

--- nimbus/procedures/forms.py
+++ nimbus/procedures/forms.py
@@ -23,3 +23,22 @@ class ProcedureForm(forms.ModelForm):
                   'pool_retention_time',
                   'name')
         exclude = ('active', 'pool_size', 'pool_name')
+
+
+class ProcedureEditForm(forms.ModelForm):
+
+    pool_retention_time = forms.IntegerField(label=_("Retention Time (days)"), min_value=1, max_value=3650)
+    fileset = forms.models.ModelChoiceField(label=_("Fileset"), queryset=FileSet.objects.filter(is_model=True))
+    schedule = forms.models.ModelChoiceField(label=_("Schedule"), queryset=Schedule.objects.filter(is_model=True))
+
+    class Meta:
+        model = Procedure
+        fields = ('active',
+                  'name',
+                  'computer',
+                  'schedule',
+                  'fileset',
+                  'storage',
+                  'pool_retention_time')
+        exclude = ('pool_size', 'pool_name')
+
--- nimbus/procedures/views.py
+++ nimbus/procedures/views.py
@@ -23,20 +23,26 @@ from nimbus.pools.models import Pool
 from nimbus.shared.views import render_to_response
 from nimbus.shared.forms import form, form_mapping
 from nimbus.shared.enums import days as days_enum, weekdays as weekdays_enum, levels as levels_enum
-from nimbus.procedures.forms import ProcedureForm
+from nimbus.procedures.forms import ProcedureForm, ProcedureEditForm
 
+def procedure2dict(procedure):
+    return {'name': procedure.name,
+            'computer': procedure.computer,
+            'pool_retention_time': procedure.pool_retention_time,
+            'active': procedure.active,
+            'schedule': procedure.schedule,
+            'fileset': procedure.fileset,
+            'storage': procedure.storage}
 
 @login_required
 def add(request):
     title = u"Adicionar backup"
     lforms = [ProcedureForm(prefix="procedure")]
-    content = {'title':u'Criar Backup',
+    content = {'title': title,
               'forms':lforms}
     if request.method == "POST":
         print request.POST
         lforms = [ProcedureForm(request.POST, prefix="procedure")]
-        content = {'title':u'Criar Backup',
-                  'forms':lforms}
         data = copy(request.POST)
         procedure_form = ProcedureForm(data, prefix="procedure")
         if procedure_form.is_valid():
@@ -45,6 +51,7 @@ def add(request):
             return redirect('/procedures/list')
         else:
             messages.error(request, "O procedimento de backup não foi criado devido aos seguintes erros")
+            content['forms'] = [procedure_form]
             return render_to_response(request, "add_procedure.html", content)
     return render_to_response(request, "add_procedure.html", content)
 
@@ -52,21 +59,28 @@ def add(request):
 @login_required
 def edit(request, procedure_id):
     p = get_object_or_404(Procedure, pk=procedure_id)
-    return render_to_response(request, 'add_procedure.html', {'procedure': p})
+    title = u"Editando %s" % p.name
+    lforms = [ProcedureEditForm(prefix="procedure", initial=procedure2dict(p))]
+    content = {'title': title,
+              'forms':lforms,
+              'id': procedure_id,
+              'schedule': p.schedule.name,
+              'fileset': p.fileset.name}
+    if request.method == "POST":
+        print request.POST
+        data = copy(request.POST)
+        procedure_form = ProcedureEditForm(data, instance=p, prefix="procedure")
+        if procedure_form.is_valid():
+            procedure_form.save()
+            messages.success(request, "Procedimento alterado com sucesso")
+            return redirect('/procedures/list')
+        else:
+            messages.error(request, "O procedimento de backup não foi criado devido aos seguintes erros")
+            content['forms'] = [procedure_form]
+            return render_to_response(request, "edit_procedure.html", content)
 
+    return render_to_response(request, 'edit_procedure.html', content)
 
-
-
-# @login_required
-# def edit(request, object_id):
-#     extra_context = {'title': u"Editar procedimento"}
-#     return create_update.update_object(request, 
-#                                        object_id = object_id,
-#                                        model = Procedure,
-#                                        form_class = form(Procedure),
-#                                        template_name = "base_procedures.html",
-#                                        extra_context = extra_context,
-#                                        post_save_redirect = "/procedures/list")
 
 @login_required
 def delete(request, object_id):
