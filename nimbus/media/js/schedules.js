6fd3153028c807c2583bec83087c22207d8edb24
Fabio <fabio.pacheco@veezor.com>
2011-06-04 15:17:49 -0300

    Correção no javascript que preenche os formularios de dias do mes e da semana

--- nimbus/media/js/schedules.js
+++ nimbus/media/js/schedules.js
@@ -1,88 +1,22 @@
 $(document).ready(function(){
-	var month_append = "";
 	$(".month_checkbox").click(function(){
-		if (this.checked == true)
-		{
-			var value = this.value;
-			if ($("#id_month-days").val() == "")
-			{
-				month_append = value;
-			}
-			else
-			{
-				month_append = $("#id_month-days").val() + "," + value;
-			}
-			$("#id_month-days").val(month_append);
-		}
-		else
-		{
-			var value = this.value;
-			var month_value = $("#id_month-days").val();
-			var remove = month_value.replace(value+",", "");
-			$("#id_month-days").val(remove);
-		}
+    	var month_array = new Array();
+	    var month_days = $(".month_checkbox");
+	    for (var i = 0; i < 31; i++) {
+	        if (month_days[i].checked == true) {
+                month_array.push(i+1)
+            }
+            $("#id_month-days").val(month_array);
+        }
 	});
-	var week_append = "";
 	$(".week_checkbox").click(function(){
-		if (this.checked == true)
-		{
-			var value = this.value;
-			if ($("#id_week-days").val() == "")
-			{
-				week_append = value;
-			}
-			else
-			{
-				week_append = $("#id_week-days").val() + "," + value;
-			}
-			$("#id_week-days").val(week_append);
-		}
-		else
-		{
-			var value = this.value;
-			var week_value = $("#id_week-days").val();
-			var remove = week_value.replace(value+",", "");
-			$("#id_week-days").val(remove);
-		}
+    	var week_array = new Array();
+	    var week_days = $(".week_checkbox");
+	    for (var i = 0; i < 7; i++) {
+	        if (week_days[i].checked == true) {
+                week_array.push(i+1)
+            }
+            $("#id_week-days").val(week_array);
+        }
 	});
-    function commit_settings(is_model){
-    	console.log(is_model);
-        var dataString = "";
-        var month_days = "";
-        var week_days = "";
-        $.each($('#schedule_new').serializeArray(), function(i, field) {
-            if (field.name == 'schedule.monthly.day')
-            {
-                month_days += field.value+",";
-            }
-            else if (field.name == 'schedule.weekly.day')
-            {
-                week_days += field.value+",";
-            }
-            else
-            {
-                dataString += field.name+"="+field.value+"&";
-            }
-        });
-        dataString += "schedule.monthly.day="+month_days+"&schedule.weekly.day="+week_days+"&schedule-is_model="+is_model;
-        $.ajax({
-            type: "POST",
-            url: "/schedules/add/",
-            data: dataString,
-            success: function() {
-                alert("Cadastro efetuado com MUITO MUITO sucesso!");
-                //$(document).trigger('close.facebox');
-            },
-            error: function(){
-                alert('Houve um problema com a requisição');
-                //$(document).trigger('close.facebox');
-            }
-        });
-    };
-    $(".use_it").click(function(){
-        commit_settings(0);
-    });
-    $(".use_and_save_it").click(function(){
-        commit_settings(1);
-    });
 });
