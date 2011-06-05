1b37e3fb807bc1f35379f58c4e115a3416decf66
Fabio <fabio.pacheco@veezor.com>
2011-06-04 23:02:43 -0300

    Começando formulários de edição de schedule

--- nimbus/schedules/views.py
+++ nimbus/schedules/views.py
@@ -43,133 +43,60 @@ from nimbus.schedules.models import Schedule
 #             return render_to_response(request, "edit_procedure.html", content)
 #     return render_to_response(request, 'edit_procedure.html', content)
 
+
 def edit(request, object_id):
     s = get_object_or_404(Schedule, pk=object_id)
-    title = u"Editando '%s'" % s.name
-    lforms = forms.ScheduleForm(prefix="schedule", instance=s)
-    schedule_forms = forms.make_schedule_form_container()
-    schedule_forms.get()
+    print s.if_week()
+    schedule_form = forms.ScheduleForm(prefix="schedule", instance=s)
+    month_form = forms.MonthForm(prefix="month", instance=s.if_month())
+    week_form = forms.WeekForm(prefix="week", instance=s.if_week())
+    day_form = forms.DayForm(prefix="day", instance=s.if_day())
+    hour_form = forms.HourForm(prefix="hour", instance=s.if_hour())
     days_range = range(1, 32)
-    weekdays_range = {0:'Domingo',
-                      1:'Segunda',
-                      2:'Terca',
-                      3:'Quarta',
-                      4:'Quinta',
-                      5:'Sexta',
+    weekdays_range = {0:'Domingo', 1:'Segunda-feira', 2:'Terça-feira',
+                      3:'Quarta-feira', 4:'Quinta-feira', 5:'Sexta-feira',
                       6:'Sabado'}
     end_days_range = [5, 10, 15, 20, 25, 30]
-    extra_content = {'title': title,
-                     'levels': levels,
-                     'forms': [lforms],
-                     'formset': schedule_forms,
-                     'days': days_range,
-                     'end_days': end_days_range,
-                     'weekdays': weekdays_range}
-    return render_to_response('edit_schedules.html', extra_content)
+    content = {'title':u'Criar Agendamento',
+               'schedule_form':schedule_form,
+               'month_form': month_form,
+               'week_form': week_form,
+               'day_form': day_form,
+               'hour_form': hour_form,
+               'days':days_range,
+               'end_days':end_days_range,
+               'weekdays':weekdays_range,
+               'messages':[]
+              }
+    return render_to_response(request, 'add_schedule.html', content)
 
-# def render(request, object_id=0):
-#     lforms = [ forms.ScheduleForm(prefix="schedules", initial={'computer':object_id}) ]
-#     content = {'title':u'Criar Backup',
-#                'forms':lforms,
-#                'computer_id':object_id}
-#     return render_to_response("backup_add.html", content)
-# 
-# def insert_schedule(POST_data):
-#     if POST_data.has_key('schedule-name') and (POST_data['schedule-name'] != ''):
-#         schedule_data = {'schedule-name': POST_data['schedule-name']}
-#     else:
-#         sugested = []
-#         if POST_data.has_key('schedule.hourly.active'):
-#             sugested.append('hora em hora')
-#         if POST_data.has_key('schedule.dayly.active'):
-#             sugested.append('diário')
-#         if POST_data.has_key('week.active'):
-#             sugested.append('semanal')
-#         if POST_data.has_key('schedule.monthly.active'):
-#             sugested.append('mensal')
-#         if len(sugested) > 1:
-#             sugested_name = "%s e %s" % (', '.join(sugested[0:-1]), sugested[-1])
-#         elif len(sugested) == 1:
-#             sugested_name = sugested[0]
-#         else:
-#             return False
-#         schedule_data = {'schedule-name': sugested_name.capitalize()}
-#     schedule_data["schedule-is_model"] = POST_data["schedule-is_model"]
-#     schedule_form = forms.ScheduleForm(schedule_data, prefix="schedule")
-#     if schedule_form.is_valid():
-#         new_schedule = schedule_form.save()
-#         print "deu certo"
-#         return new_schedule
-#     else:
-#         # tratar na interface
-#         # messages.warning(request, procedure_form.errors)
-#         # return render_to_response(request, "schedules_new.html", locals())
-#         print "nao deu certo"
-#         return schedule_form.errors
-# 
-# def insert_monthly(data, schedule):
-#     if data.has_key('schedule.monthly.active'):
-#         days = data['schedule.monthly.day']
-#         if (len(days) > 1) and (days[-1] == ","):
-#             days = days[0:-1]
-#         month_form = forms.MonthlyForm({'schedule': schedule.id,
-#                                         'days': days,
-#                                         'hour': data['month-hour'],
-#                                         'level': data['month-level']})
-#         if month_form.is_valid():
-#             month_form.save()
-#             return u"Agendamento mensal adicionado com sucesso"
-#         else:
-#             # tratar na interface
-#             return month_form.errors
-#     else:
-#         return None
-# 
-# def insert_weekly(data, schedule):
-#     if data.has_key('week.active'):
-#         days = data['schedule.weekly.day']
-#         if days[-1] == ",":
-#             days = days[0:-1]
-#         week_form = forms.WeeklyForm({'schedule': schedule.id,
-#                                       'days': days,
-#                                       'hour': data['week-hour'],
-#                                       'level': data['week-level']})
-#         if week_form.is_valid():
-#             week_form.save()
-#             return u"Agendamento semanal adicionado com sucesso"
-#         else:
-#             # tratar na interface
-#             return week_form.errors
-#     else:
-#         return None
-# 
-# def insert_daily(data, schedule):
-#     if data.has_key('schedule.dayly.active'):
-#         day_form = forms.DailyForm({'schedule': schedule.id,
-#                                     'hour': data['day-hour'],
-#                                     'level': data['day-level']})
-#         if day_form.is_valid():
-#             day_form.save()
-#             return u"Agendamento diário adicionado com sucesso"
-#         else:
-#             # tratar na interface
-#             return day_form.errors
-#     else:
-#         return None
-#         
-# def insert_hourly(data, schedule):
-#     if data.has_key('schedule.hourly.active'):
-#         hour_form = forms.HourlyForm({'schedule': schedule.id,
-#                                     'minute': data['hour-minute'],
-#                                     'level': data['hour-level']})
-#         if hour_form.is_valid():
-#             hour_form.save()
-#             return u"Agendamento de hora em hora adicionado com sucesso"
-#         else:
-#             # tratar na interface
-#             return hour_form.errors
-#     else:
-#         return None
+
+def edit___(request, procedure_id):
+    p = get_object_or_404(Procedure, pk=procedure_id)
+    title = u"Editando '%s'" % p.name
+    partial_form = ProcedureEditForm(prefix="procedure", instance=p)
+    lforms = [partial_form]
+    content = {'title': title,
+              'forms':lforms,
+              'id': procedure_id,
+              'schedule': p.schedule,
+              'fileset': p.fileset}
+    if request.method == "POST":
+        data = copy(request.POST)
+        if data['procedure-schedule'] == u"":
+            data['procedure-schedule'] = u"%d" % p.schedule.id
+        if data['procedure-fileset'] == u"":
+            data['procedure-fileset'] = u"%d" % p.fileset.id
+        procedure_form = ProcedureEditForm(data, instance=p, prefix="procedure")
+        if procedure_form.is_valid():
+            procedure_form.save()
+            messages.success(request, "Procedimento '%s' alterado com sucesso" % p.name)
+            return redirect('/procedures/list')
+        else:
+            messages.error(request, "O procedimento de backup não foi criado devido aos seguintes erros")
+            content['forms'] = [procedure_form]
+            return render_to_response(request, "edit_procedure.html", content)
+    return render_to_response(request, 'edit_procedure.html', content)
 
 
 def add_schedule(request):
