6a659d01990fefd4f3cc8604a5e6041bbd2a541c
Rodrigo Araújo <fenrrir@gmail.com>
2011-06-06 12:19:30 -0300

    Removendo antigo gateway e biblioteca cliente

--- webservices/gateway/nimbusgate/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from gateway.nimbusgate.models import OperationLog, UserBucket, Operation
-
-admin.site.register(OperationLog)
-admin.site.register(UserBucket)
-admin.site.register(Operation)
