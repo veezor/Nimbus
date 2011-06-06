7e937ca354904786f71dfe27acc7419660f2d9ff
Fabio <fabio.pacheco@veezor.com>
2011-06-06 12:26:20 -0300

    Rollback para o commit 'Definido valor inicial para retention time em 10 dias'

--- nimbus/procedures/models.py
+++ nimbus/procedures/models.py
@@ -1,204 +1,179 @@
 #!/usr/bin/env python
 # -*- coding: UTF-8 -*-
 
+import logging
+from os.path import join, exists
+from django.utils.translation import ugettext as _
 
-from os import path
-
-from django.db import models
+from django.db import models, connections
 from django.conf import settings
-from django.db.models.signals import post_save, post_delete
-
+from django.db.models.signals import post_save, post_delete, pre_save 
+from pybacula import BConsoleInitError
 from nimbus.base.models import BaseModel
-from nimbus.shared import signals, utils, enums, fields
+from nimbus.computers.models import Computer
+from nimbus.storages.models import Storage
+from nimbus.filesets.models import FileSet
+from nimbus.schedules.models import Schedule
+from nimbus.bacula.models import Media
+# from nimbus.pools.models import Pool
 from nimbus.libs.template import render_to_file
+from nimbus.libs.bacula import Bacula
+from nimbus.offsite.models import Offsite
+from nimbus.offsite.models import is_active
+from nimbus.libs import offsite
+from nimbus.bacula.models import Job, File
+from nimbus.shared import utils, enums, signals, fields
 
-
-LEVELS = tuple( (l,l) for l in enums.levels )  
-WEEKDAYS = tuple( (d,d) for d in enums.weekdays )
-MONTHDAYS = tuple( (d,d) for d in enums.days )
-
-
-class BackupLevel(BaseModel):
-    name = models.CharField(max_length=255, unique=True, null=False)
-
-    def __unicode__(self):
-        return self.name
-
-
-class Schedule(BaseModel):
-    name = models.CharField(u'Nome qualquer', max_length=255, null=False, blank=False)
-    is_model = models.BooleanField(default=False, null=False)
-
-    def __unicode__(self):
-        return self.name
-
-
-class Month(BaseModel):
-    active = models.BooleanField(default=False, blank=True, null=False)
-    schedule = models.OneToOneField(Schedule)
-    days = models.CommaSeparatedIntegerField(null=False, max_length=255)
-    hour = models.TimeField()
-    level = models.ForeignKey(BackupLevel)
-
-    def __unicode__(self):
-        return self.schedule.name
-
-
-class Week(BaseModel):
-    active = models.BooleanField(default=False, blank=True, null=False)
-    schedule = models.OneToOneField(Schedule)
-    days = models.CommaSeparatedIntegerField(null=False, max_length=255)
-    hour = models.TimeField()
-    level = models.ForeignKey(BackupLevel)
-
-    def __unicode__(self):
-        return self.schedule.name
-
-
-class Day(BaseModel):
-    active = models.BooleanField(default=False, blank=True, null=False)
-    schedule = models.OneToOneField(Schedule)
-    hour = models.TimeField()
-    level = models.ForeignKey(BackupLevel)
-
-    def __unicode__(self):
-        return self.schedule.name
-
-
-class Hour(BaseModel):
-    active = models.BooleanField(default=False, blank=True, null=False)
-    schedule = models.OneToOneField(Schedule)
-    minute = models.PositiveSmallIntegerField()
-    level = models.ForeignKey(BackupLevel)
-
-    def __unicode__(self):
-        return self.schedule.name
-
-
-# class Schedule(BaseModel):
-#     name = models.CharField(max_length=255, unique=True, null=False,
-#                              validators=[fields.check_model_name])
+# Nao serao usados profiles por enquanto. Passei os relacionamentos
+# para Procedures
+# class Profile(models.Model):
 # 
-#     def get_triggers(self):
-#         return list(self.hourly_set.get_query_set()) +\
-#                 list(self.daily_set.get_query_set()) +\
-#                 list(self.monthly_set.get_query_set()) +\
-#                 list(self.weekly_set.get_query_set())
-# 
-# 
-#     def get_monthly_hour(self): #FIX: remove this
-#         if self.monthly_set.count():
-#             return self.monthly_set.all()[0].hour
-# 
-#     def get_weekly_hour(self): #FIX: remove this
-#         if self.weekly_set.count():
-#             return self.weekly_set.all()[0].hour
-# 
-# 
-#     def get_runs(self):
-#         return [ trigger.get_run() for trigger in self.get_triggers() ]
-# 
+#     name = models.CharField(max_length=255, unique=True, blank=True, null=False)
+#     # storage = models.ForeignKey(Storage, null=False, blank=False)
+#     # fileset = models.ForeignKey(FileSet, null=False, blank=False)
+#     # schedule = models.ForeignKey(Schedule, null=False, blank=False)
 # 
 #     def __unicode__(self):
 #         return self.name
 
 
+class Procedure(BaseModel):
+    pool_name = models.CharField(max_length=255)
+    pool_size = models.FloatField(blank=False, null=False, default=5242880,
+                                  editable=False)
+    pool_retention_time = models.IntegerField(verbose_name=_("Retention Time (days)"),
+                                              blank=False, null=False,
+                                              default=30)
+    computer = models.ForeignKey(Computer, verbose_name=_("Computer"),
+                                 blank=False, null=False)
+    offsite_on = models.BooleanField(default=False, blank=False, null=False,
+                                     editable=is_active(Offsite))
+    active = models.BooleanField(default=True, blank=True, null=False)
+    schedule = models.ForeignKey(Schedule, verbose_name=_("Schedule"),
+                                 related_name='schedule')
+    fileset = models.ForeignKey(FileSet, verbose_name=_("Fileset"),
+                                related_name='fileset')
+    storage = models.ForeignKey(Storage, verbose_name=_("Storage"), null=False,
+                                blank=False)
+    name = models.CharField(verbose_name=_("Name"), max_length=255, blank=False,
+                            null=False)
 
-# class TriggerBase(models.Model):
-#     schedule = models.ForeignKey(Schedule, null=False, blank=False)
-#     level = models.CharField(max_length="25", null=False, 
-#                              blank=False, choices=LEVELS)
-#     hour = models.TimeField( null=False, blank = False) 
-# 
-#     @classmethod
-#     def type_name(cls):
-#         return cls.__name__.lower()
-# 
-#     def __unicode__(self):
-#         return self.get_run()
-# 
-#     class Meta:
-#         abstract = True
-# 
-# 
-# class Daily(TriggerBase):
-#     
-#     def get_run(self):
-#         return u"%s at %s" % ( self.type_name(),
-#                                self.hour.strftime("%H:%M") )
-# 
-# 
-# 
-# class Hourly(TriggerBase):
-#     
-#     def get_run(self):
-#         return u"%s at 00:%s" % ( self.type_name(),
-#                                   self.hour.strftime("%M") )
-# 
-# 
-# class Monthly(TriggerBase):
-#     day = models.IntegerField(null=False, blank=False, choices=MONTHDAYS)
-# 
-# 
-#     def get_run(self):
-#         return u"%s %d at %s" % ( self.type_name(), 
-#                                   int(self.day),
-#                                   self.hour.strftime("%H:%M") )
-# 
-# 
-# 
-# class Weekly(TriggerBase):
-#     day = models.CharField(null=False, blank=False,
-#                            max_length=4, choices=WEEKDAYS)
-# 
-# 
-#     def get_run(self):
-#         return u"%s %s at %s" % ( self.type_name(), 
-#                                   self.day,
-#                                   self.hour.strftime("%H:%M") )
-
-
-
-
-
-
-
-
-def update_schedule_file(schedule):
-
-    name = schedule.bacula_name
-
-    filename = path.join( settings.NIMBUS_SCHEDULES_DIR, 
-                          name)
-
-    render_to_file( filename,
-                    "schedule",
-                    name=name,
-                    runs=schedule.get_runs() )
-
-
-
-def remove_schedule_file(schedule):
-    name = schedule.bacula_name
-    filename = path.join( settings.NIMBUS_SCHEDULES_DIR, name)
-    utils.remove_or_leave(filename)
-
-
-
-def update_schedule(trigger):
-    update_schedule_file(trigger.schedule)
+    def fileset_bacula_name(self):
+        return self.fileset.bacula_name
+        # return self.profile.fileset.bacula_name
     
+    def restore_bacula_name(self):
+        return "%s_restorejob" % self.uuid.uuid_hex
+        
+    def schedule_bacula_name(self):
+        return self.schedule.bacula_name
+        # return self.profile.schedule.bacula_name
 
-# signals.connect_on( update_schedule_file, Schedule, post_save)
-# signals.connect_on( remove_schedule_file, Schedule, post_delete)
-# 
-# 
-# signals.connect_on( update_schedule, Monthly, post_save)
-# signals.connect_on( update_schedule, Daily, post_save)
-# signals.connect_on( update_schedule, Weekly, post_save)
-# signals.connect_on( update_schedule, Hourly, post_save)
-# 
-# signals.connect_on( update_schedule, Monthly, post_delete)
-# signals.connect_on( update_schedule, Daily, post_delete)
-# signals.connect_on( update_schedule, Weekly, post_delete)
-# signals.connect_on( update_schedule, Hourly, post_delete)
+    def storage_bacula_name(self):
+        return self.storage.bacula_name
+        # return self.profile.storage.bacula_name
+
+    def pool_bacula_name(self):
+        return self.pool_name
+
+    def last_success_date(self):
+        return Job.objects.filter(name=self.bacula_name,jobstatus='T')\
+                .order_by('-endtime')[0]
+
+    def restore_jobs(self):
+        return Job.objects.filter(client__name=self.computer.bacula_name,
+                                  fileset__fileset=self.fileset_bacula_name,
+                                  jobstatus='T').order_by('-endtime').distinct()[:15]
+
+    def get_backup_jobs_between(self, start, end):
+        jobs = Job.objects.filter(realendtime__range=(start,end), 
+                                  jobfiles__gt=0,
+                                  jobstatus='T',
+                                  type='B',
+                                  name=self.bacula_name)\
+                                                .order_by('-endtime').distinct()
+        return jobs
+
+    def __unicode__(self):
+        return self.name 
+
+    def run(self):
+        bacula = Bacula()
+        bacula.run_backup(self.bacula_name, 
+                          client_name=self.computer.bacula_name)
+   
+    @classmethod
+    def disable_offsite(cls):
+        cls.objects.filter(offsite_on=True).update(offsite_on=False)
+
+    @staticmethod
+    def list_files(jobid, path="/"):
+       bacula = Bacula()
+       return bacula.list_files(jobid, path)
+
+    @staticmethod
+    def search_files(jobid, pattern):
+        files = File.objects.filter(
+                models.Q(filename__name__icontains=pattern) | models.Q(
+                path__path__icontains=pattern), job__jobid=jobid).distinct()
+        files = [f.fullname for f in files]
+        files.sort()
+        return files
+
+
+def update_procedure_file(procedure):
+    """Procedure update file"""
+    name = procedure.bacula_name
+    filename = join(settings.NIMBUS_JOBS_DIR, name)
+    render_to_file(filename,
+                   "job",
+                   name=name,
+                   schedule=procedure.schedule_bacula_name(),
+                   storage=procedure.storage_bacula_name(),
+                   fileset=procedure.fileset_bacula_name(),
+                   priority="10",
+                   offsite=procedure.offsite_on,
+                   active=procedure.active,
+                   offsite_param="--upload-requests %v",
+                   client=procedure.computer.bacula_name,
+                   pool=procedure.pool_bacula_name() )
+    if not exists(settings.NIMBUS_RESTORE_FILE):
+        render_to_file(settings.NIMBUS_RESTORE_FILE,
+                       "restore",
+                       name=name + "restore",
+                       storage=procedure.storage_bacula_name(),
+                       fileset=procedure.fileset_bacula_name(),
+                       client=procedure.computer.bacula_name,
+                       pool=procedure.pool_bacula_name())
+
+def remove_procedure_file(procedure):
+    """remove procedure file"""
+    base_dir,filepath = utils.mount_path(procedure.bacula_name,
+                                         settings.NIMBUS_JOBS_DIR)
+    utils.remove_or_leave(filepath)
+
+def remove_procedure_volumes(procedure):
+    pool_name = procedure.pool_bacula_name()
+    medias = Media.objects.filter(pool__name=pool_name).distinct()
+    volumes = [m.volumename for m in medias]
+    try:
+        bacula = Bacula()
+        bacula.purge_volumes(volumes, pool_name)
+        bacula.truncate_volumes(pool_name)
+        bacula.delete_pool(pool_name)
+    except BConsoleInitError, error:
+        logger = logging.getLogger(__name__)
+        logger.exception("Erro na comunicação com o bacula")
+    if procedure.offsite_on:
+        remote_manager = offsite.RemoteManager()
+        remote_manager.create_deletes_request( volumes )
+
+def offsiteconf_check(procedure):
+    offsite = Offsite.get_instance()
+    if not offsite.active:
+        procedure.offsite_on = False
+
+signals.connect_on( offsiteconf_check, Procedure, pre_save)
+signals.connect_on( update_procedure_file, Procedure, post_save)
+signals.connect_on( remove_procedure_file, Procedure, post_delete)
+signals.connect_on( remove_procedure_volumes, Procedure, post_delete)
