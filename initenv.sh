bd2e1acf44649e49483c791655deea7f3d568db8
Rodrigo Araújo <fenrrir@gmail.com>
2011-06-08 10:33:03 -0300

    FIX: adicionando diretorio apps ao initenv.sh

--- initenv.sh
+++ initenv.sh
@@ -8,4 +8,4 @@ then
 fi
 
 # sobrescrevendo PYTHONPATH
-export PYTHONPATH=$PYTHONPATH:$(pwd)/libs:$(pwd)/webservices/manager
+export PYTHONPATH=$PYTHONPATH:$(pwd)/libs:$(pwd)/webservices/manager:$(pwd)/apps
