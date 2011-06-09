db40f0003ef566c0f0cfd8ed797a246481d3383c
Fabio <fabio.pacheco@veezor.com>
2011-06-09 11:56:08 -0300

    Correções do formulário de procedures

--- nimbus/procedures/forms.py
+++ nimbus/procedures/forms.py
@@ -13,19 +13,24 @@ class ProcedureForm(forms.ModelForm):
         # Se houver apenas uma opcao num ChoiceField a opção NULL sera removida
         for field in self.fields:
             if isinstance(self.fields[field], forms.models.ModelChoiceField):
-                if len(self.fields[field].choices) == 1:
-                    remove_null_choice(self, [field])
+                not_model_filter(field, self.fields[field])
+                if field not in ['schedule', 'fileset']:
+                    remove_null_choice(self.fields[field])
+                else:
+                    self.fields[field].empty_label = u"-ou escolha um modelo-"
 
     pool_retention_time = forms.IntegerField(label=_("Retention Time (days)"),
                                              min_value=1, max_value=3650,
                                              initial=10,
                                              widget=forms.HiddenInput())
     fileset = forms.models.ModelChoiceField(label=_("Fileset"),
-                                queryset=FileSet.objects.filter(is_model=True))
+                                            queryset=FileSet.objects.all(),
+                                            empty_label = u"-ou escolha um modelo-")
     schedule = forms.models.ModelChoiceField(label=_("Schedule"),
-                                queryset=Schedule.objects.filter(is_model=True))
-    fileset.empty_label = u"-ou escolha um modelo-"
-    schedule.empty_label = u"-ou escolha um modelo-"
+                                             queryset=Schedule.objects.all(),
+                                             empty_label = u"-ou escolha um modelo-")
+    # fileset.empty_label = u"-ou escolha um modelo-"
+    # schedule.empty_label = u"-ou escolha um modelo-"
 
     class Meta:
         model = Procedure
@@ -53,10 +58,10 @@ class ProcedureEditForm(forms.ModelForm):
                                             label=_("Schedule"), required=False,
                                             queryset=Schedule.objects.filter(
                                                 Q(is_model=True) | Q(id=s_id)))
-        remove_null_choice(self, ['schedule', 'fileset', 'storage', 'computer'])
+#        remove_null_choice(self, ['schedule', 'fileset', 'storage', 'computer'])
                                                 
     pool_retention_time = forms.IntegerField(label=_("Retention Time (days)"),
-                                             min_value=1, max_value=3650, widget=forms.HiddenInput())
+                                             min_value=1, widget=forms.HiddenInput())
                                 
     class Meta:
         model = Procedure
@@ -70,11 +75,29 @@ class ProcedureEditForm(forms.ModelForm):
         exclude = ('pool_size', 'pool_name')
 
 
-def remove_null_choice(current_form, fields):
-    for field in fields:
+def remove_null_choice(field_object):
+    current_choices = field_object.choices
+    print len(current_choices)
+    if len(current_choices) == 1:
         choices = []
-        for choice in current_form.fields[field].choices:
+        for choice in current_choices:
             if choice[0] != u'':
                 choices.append(choice)
-        current_form.fields[field].choices = choices
+        field_object.choices = choices
 
+
+def not_model_filter(field_name, field_object):
+    # choice_forms = [{'field': 'schedule', 'object': Schedule},
+    #          {'field': 'fileset', 'object': FileSet}]
+    # for cform in choice_forms:
+    choice_forms = {'schedule': Schedule, 'fileset': FileSet}
+    if choice_forms.has_key(field_name):    
+        choices = []
+        current_choices = field_object.choices
+        for choice in current_choices:
+            if choice[0] != u'':
+                if choice_forms[field_name].objects.get(id=choice[0]).is_model:
+                    choices.append(choice)
+            else:
+                choices.append(choice)
+        field_object.choices = choices
