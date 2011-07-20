9299e9039c5825d04c9508611dbe9ab57a34589a
Rodrigo Araújo <fenrrir@gmail.com>
2011-07-20 15:04:08 -0300

    Otimização: diminuição no número de queries feitas pelo middleware de wizard

--- nimbus/wizard/middleware.py
+++ nimbus/wizard/middleware.py
@@ -17,20 +17,24 @@ class Wizard(object):
             raise MiddlewareNotUsed("wizard completed")
 
     def process_request(self, request):
+
+        
+        if not self.is_restricted_url(request):
+            return None
+
         wizard = models.Wizard.get_instance()
 
         if wizard.has_completed():
             return None
         else:
-            if self.grant_access(request):
-                return None
-            else:
-                return redirect('nimbus.wizard.views.license')
+            return redirect('nimbus.wizard.views.license')
 
-    def grant_access(self, request):
+
+    def is_restricted_url(self, request):
         path = request.META['PATH_INFO']
         if path.startswith("/wizard") or\
                 path.startswith("/media") or\
                 path.startswith("/recovery") or\
                 'ajax' in path:
-            return True
+            return False
+        return True
