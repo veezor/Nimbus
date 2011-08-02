962ab7e012beb8d030f053a5af11c65f237d5e62
Rodrigo Araújo <rodrigo.araujo@veezor.com>
2011-08-02 11:30:24 -0300

    FIX: fix erro de js com o relogio da interface

--- nimbus/media/lib/js/custom.js
+++ nimbus/media/lib/js/custom.js
@@ -211,13 +211,16 @@ $(document).ready(function(){
 	})
 	// $(".sparklines").sparkline('html', {width: "300", height: "150px" });
 	
-	var actual_time = $('#actual_time').html().split(':');
-	var hours = actual_time[0];
-	var minutes = actual_time[1];
-	var seconds = actual_time[2];
-	data_hora = new Date(2010, 10, 10, hours, minutes, seconds);
-	update_time();
-	
+	var actual_time_html = $('#actual_time').html();
+    if (actual_time_html != null){
+        var actual_time.split(':');
+        var hours = actual_time[0];
+        var minutes = actual_time[1];
+        var seconds = actual_time[2];
+        data_hora = new Date(2010, 10, 10, hours, minutes, seconds);
+        update_time();
+    }
+        
 	$(".date_picker").mask('99/99/9999');
 	$(".date_picker").bind("blur focus change click keyup", function(){
 	   $(this).mask('99/99/9999');
