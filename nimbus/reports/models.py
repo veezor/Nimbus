042789320c8955133ac3719c423a26259342b467
Rodrigo Araújo <rodrigo.araujo@veezor.com>
2011-09-01 15:28:35 -0300

    FIX: email timeout

--- nimbus/reports/models.py
+++ nimbus/reports/models.py
@@ -3,6 +3,7 @@
 
 
 import re
+import socket
 import fileinput
 
 from django.db import models
@@ -19,7 +20,8 @@ EMAIL_TEST_SUBJECT=u"Nimbus: Email de teste"
 EMAIL_TEST_MESSAGE=u"""
 Este email foi enviado a seu pedido para verificação da configuração de email do Nimbus.
 """
-
+DEFAULT_SOCKET_TIMEOUT=10
+socket.setdefaulttimeout(DEFAULT_SOCKET_TIMEOUT)
 
 
 FIELD_RE = ":\s*(.*)"
