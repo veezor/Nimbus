0f06ba172f2718a3ccb6c910d4aad8382dba914e
Rodrigo Araújo <rodrigo.araujo@veezor.com>
2012-01-12 15:31:27 -0300

    Informação de rate atual no ratelimiter

--- nimbus/offsite/S3.py
+++ nimbus/offsite/S3.py
@@ -39,6 +39,7 @@ class RateLimiter(object):
         self.update_rate_limit_time = update_rate_limit_time
         self._last_update_rate_limit = None
         self._transferred_size = 0
+        self.rate = None
 
 
     def reset(self, rate_limit):
@@ -56,6 +57,7 @@ class RateLimiter(object):
         if elapsed_time and self.rate_limit > 0:
 
             rate = t_size / elapsed_time
+            self.rate = rate
             expected_time = t_size / self.rate_limit
             sleep_time = expected_time - elapsed_time
 
