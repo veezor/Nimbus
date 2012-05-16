55b663b04e042e44724c18e33bde0d1747397a6f
Rodrigo Araújo <rodrigo.araujo@veezor.com>
2012-05-16 16:28:10 -0300

    i18n da app reports

--- nimbus/reports/forms.py
+++ nimbus/reports/forms.py
@@ -2,20 +2,22 @@
 # -*- coding: UTF-8 -*-
 
 from django import forms
+from django.utils.translation import ugettext as _
+from django.utils.translation import ugettext_lazy as __
 
 from nimbus.reports.models import EmailConf
 
 
 class EmailConfForm(forms.ModelForm):
-    email_password = forms.CharField(label=u'Senha', required=False,
+    email_password = forms.CharField(label=__(u'Password'), required=False,
                                      widget=forms.PasswordInput(render_value=False))
 
 
     def is_valid(self):
         data = self.data.copy()
         if not 'active' in data:
-            data['send_to'] = 'seuemail@suaempresa.com'
-            data['email_host'] = 'suaempresa.com'
+            data['send_to'] = _('youruser@yourcompany.com')
+            data['email_host'] = _('yourcompany.com')
             data['email_port'] = '25'
         self.data = data
         return super(EmailConfForm, self).is_valid()
